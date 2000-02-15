5a40eec09aa5efd02db6e93b12ad0e911e3d31d7
scoobie <dev-null@apache.org>
2000-02-15 17:20:14 +0000

    Code Clean up
    git-svn-id: https://svn.apache.org/repos/asf/james/server@107055 13f79535-47bb-0310-9956-ffa450edef68

---
+++ trunk/src/org/apache/avalon/blocks/MailServer.java
@@ -0,0 +1,30 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included  with this distribution in *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+ 
+package org.apache.avalon.blocks;
+
+import javax.mail.internet.*;
+import java.util.*;
+import org.apache.arch.*;
+
+/**
+ * @author  Federico Barbieri <scoobie@pop.systemy.it>
+ */
+
+public interface MailServer {
+
+    public void sendMail(String sender, Vector recipients, MimeMessage msg);
+    
+    public void sendMail(String sender, Vector recipients, String body);
+
+    /* to be extended with methods like
+
+    public OutputStream sendMail(String sender, Vector recipients);
+    
+    and something to retrive mails from mailbox like javax.mail.Store etc.*/
+}
