f2d5dc8b5b0104a8c6454247ca535040c0d0da63
scoobie <dev-null@apache.org>
2000-02-26 16:56:08 +0000

    Added DNS support in classpath
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@107058 13f79535-47bb-0310-9956-ffa450edef68

--- res/run.sh
+++ res/run.sh
@@ -12,6 +12,6 @@ if [ "$JAVA_HOME" = "" ] ; then
   exit 1
 fi
 
-LOCAL_CLASSPATH=Avalon.jar;JAMES.jar;../lib/activation.jar;../lib/mail.jar;../lib/xerces_1_0_1.jar
+LOCAL_CLASSPATH=Avalon.jar;JAMES.jar;../lib/activation.jar;../lib/mail.jar;../lib/xerces_1_0_1.jar;../lib/dnsjava.jar
 
 $JAVA_HOME/bin/java -cp $CLASSPATH:$LOCAL_CLASSPATH org.apache.avalon.Main $*
