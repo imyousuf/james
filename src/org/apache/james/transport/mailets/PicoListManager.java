3aaf1d397427bf49d6d578108f8775fb376cb943
scoobie <dev-null@apache.org>
2000-06-06 02:50:28 +0000

    MailServlet -> Mailet improved processors pluggability
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@107115 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/james/transport/mailets/PicoListManager.java
@@ -0,0 +1,94 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included  with this distribution in *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+
+package org.apache.james.transport.mailets;
+
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import org.apache.java.lang.*;
+import org.apache.james.*;
+import org.apache.james.usermanager.*;
+import org.apache.avalon.interfaces.*;
+import org.apache.mail.*;
+import org.apache.james.transport.*;
+
+/**
+ * Routes a given email address to a list of members, based on simple
+ * listserv settings.
+ *
+ * Sample configuration:
+ * <pre>
+ * <servlet match="RecipientIs=test@glissando.lokitech.com" class="MiniListserv">
+ *     <subject>Test</subject>
+ *     <membersonly>true</membersonly>
+ *     <attachmentsallowed>false</attachmentsallowed>
+ *     <replytolist>true</replytolist>
+ *     <member>sergek@lokitech.com</member>
+ *     <member>sknystautas@yahoo.com</member>
+ * </servlet>
+ * </pre>
+ * @author  Serge Knystautas <sergek@lokitech.com>
+ */
+public class PicoListManager extends AbstractMailet {
+
+    private String listName;
+    private UsersRepository members;
+    private String subscribe;
+    private String unsubscribe;
+    private Logger logger;
+    private Mailet transport;
+
+    public void init () {
+        MailetContext context = getContext();
+        ComponentManager comp = context.getComponentManager();
+        logger = (Logger) comp.getComponent(Interfaces.LOGGER);
+        Configuration conf = context.getConfiguration();
+        listName = conf.getConfiguration("listName").getValue();
+        subscribe = conf.getConfiguration("subscribe", listName + "-on").getValue();
+        unsubscribe = conf.getConfiguration("unsubscribe", listName + "-off").getValue();
+        UserManager manager = (UserManager) comp.getComponent(Resources.USERS_MANAGER);
+        members = (UsersRepository) manager.getUserRepository("list-" + listName);
+        transport = (Mailet) context.get("transport");
+        logger.log("PicoListserv: " + listName + " sub " + subscribe + " unsub " + unsubscribe + "-" + transport);
+    }
+
+    public void service(Mail mail) throws Exception {
+        Vector recipients = mail.getRecipients();
+        String recipient = (String) recipients.elementAt(0);
+        if (recipients.size() != 1) {
+            logger.log("bouncing back: Cannot handle more than one recipient");
+            transport.service(mail.bounce("Cannot handle more than one recipient"));
+            mail.setState(Mail.GHOST);
+            return;
+        }
+        
+        if (Mail.getUser(recipient).equalsIgnoreCase(subscribe)) {
+            String sender = mail.getSender();
+            members.addUser(sender, "");
+            logger.log("User " + sender + " subscribed to list " + listName);
+            transport.service(mail.bounce("You have been subscribed to this list"));
+            mail.setState(Mail.GHOST);
+            return;
+        } 
+        if (Mail.getUser(recipient).equalsIgnoreCase(unsubscribe)) {
+            String sender = mail.getSender();
+            members.removeUser(sender);
+            logger.log("User " + sender + " unsubscribed to list " + listName);
+            transport.service(mail.bounce("You have been unsubscribed to this list"));
+            mail.setState(Mail.GHOST);
+            return;
+        }
+        logger.log("Return untouched mail. The address " + recipient + " cannot be handled.");
+    }
+
+    public String getServletInfo() {
+        return "PicoListManager Servlet";
+    }
+}
+
--- src/org/apache/mail/Mailet.java
+++ src/org/apache/mail/Mailet.java
@@ -11,7 +11,15 @@ package org.apache.mail;
 import org.apache.java.lang.*;
 
 /**
- * Draft of a MailServlet inteface.
+ * Draft of a Mailet inteface. The <code>service</code> perform all needed work 
+ * on the Mail object. Whatever remains at the end of the service is considered 
+ * to need futher processing and will go to the next Mailet if there is one 
+ * configured or will go to the error processor if not.
+ * Setting a Mail state (setState(String)) to Mail.GOHST or cleaning its recipient
+ * list has the same meaning that s no more processing is needed. 
+ * The service should NEVER add recipients to the Mail passed. Instead a new Mail 
+ * object should be created and delivered using the transport API.
+ * API are provided trought the MailetContext interface.
  *
  * @version 1.0.0, 24/04/1999
  * @author  Federico Barbieri   <scoobie@pop.systemy.it>
