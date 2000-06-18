86e194a53e66aec9e235a27077e487766bf17e41
Serge Knystautas <serge@apache.org>
2000-06-18 04:57:16 +0000

    Fixes to build and startup scripts to use new Xerces jar.
    git-svn-id: https://svn.apache.org/repos/asf/james/server@107127 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/res/run.sh
+++ trunk/res/run.sh
@@ -11,6 +11,6 @@ if [ "$JAVA_HOME" = "" ] ; then
   exit 1
 fi
 
-LOCAL_CLASSPATH=Avalon.jar:JAMES.jar:../lib/activation.jar:../lib/mail_1_1_3.jar:../lib/xerces_1_0_1.jar:../lib/dnsjava.jar
+LOCAL_CLASSPATH=Avalon.jar:JAMES.jar:../lib/activation.jar:../lib/mail_1_1_3.jar:../lib/xerces_1_1_1.jar:../lib/dnsjava.jar
 
 $JAVA_HOME/bin/java -cp $CLASSPATH:$LOCAL_CLASSPATH org.apache.avalon.Main $*
