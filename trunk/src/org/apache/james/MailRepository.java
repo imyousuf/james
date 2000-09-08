ba40dd230e157b829dccbe3d3c0a5038ff4c3411
charles <dev-null@apache.org>
2000-09-08 11:10:16 +0000

    Restoring MailRepository in main tree. New files only in proposal subtree.
    git-svn-id: https://svn.apache.org/repos/asf/james/server@107154 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/org/apache/james/MailRepository.java
+++ trunk/src/org/apache/james/MailRepository.java
@@ -18,11 +18,11 @@ import javax.mail.internet.*;
 import javax.mail.MessagingException;
 
 /**
- * Interface for a Repository to store Mails.
+ * Implementation of a Repository to store Mails.
  * @version 1.0.0, 24/04/1999
  * @author  Federico Barbieri <scoobie@pop.systemy.it>
  */
-public interface MailRepository extends Store.Repository {
+public class MailRepository implements Store.Repository {
 
     /**
      * Define a STREAM repository. Streams are stored in the specified
@@ -30,26 +30,122 @@ public interface MailRepository extends Store.Repository {
      */
     public final static String MAIL = "MAIL";
 
+    private Store.StreamRepository sr;
+    private Store.ObjectRepository or;
+    private String path;
+    private String name;
+    private String destination;
+    private String type;
+    private String model;
+    private Lock lock;
 
-    /** Returns the key for a mail deposited in this Repository. Not sure this belongs here, only relevant for spool repositories.
-     */
+    public MailRepository() {
+    }
 
-    public String accept();
+    public void setAttributes(String name, String destination, String type, String model) {
 
+        this.name = name;
+        this.destination = destination;
+        this.model = model;
+        this.type = type;
+    }
+        
+    public void setComponentManager(ComponentManager comp) {
 
-    /** Stores a message in this repository. Shouldn't this return the key under which it is stored? */
-    public void store(Mail mc) ;
-      
-    /** Retrieves a message given a key. At the moment, keys can be obtained  from list() in superinterface Store.Repository */
-    public Mail retrieve(String key);
+        Store store = (Store) comp.getComponent(Interfaces.STORE);
+        this.sr = (Store.StreamRepository) store.getPrivateRepository(destination, Store.STREAM, model);
+        this.or = (Store.ObjectRepository) store.getPrivateRepository(destination, Store.OBJECT, model);
+        lock = new Lock();
+    }
     
-    /** Removes a specified message */
-    public void remove(Mail mail); 
+    public String getName() {
+        return name;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public String getModel() {
+        return model;
+    }
+    
+    public String getChildDestination(String childName) {
+        return destination + childName.replace ('.', File.separatorChar) + File.separator;
+    }
+    
+    public synchronized void unlock(Object key) {
 
-    /** Removes a message identifed by key. */
-    public void remove(String key) ;
+        if (lock.unlock(key)) {
+            notifyAll();
+        } else {
+            throw new LockException("Your thread do not own the lock of record " + key);
+        }
+    }
 
+    public synchronized void lock(Object key) {
 
+        if (lock.lock(key)) {
+            notifyAll();
+        } else {
+            throw new LockException("Record " + key + " already locked by another thread");
+        }
+    }
+
+    public synchronized String accept() {
+
+        while (true) {
+            for(Enumeration e = or.list(); e.hasMoreElements(); ) {
+                Object o = e.nextElement();
+                if (lock.lock(o)) {
+                    return o.toString();
+                }
+            }
+            try {
+                wait();
+            } catch (InterruptedException ignored) {
+            }
+        }
+    }
+
+    public synchronized void store(Mail mc) {
+        try {
+            String key = mc.getName();
+            OutputStream out = sr.store(key);
+            mc.writeMessageTo(out);
+            out.close();
+            or.store(key, mc);
+            notifyAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Exception caught while storing Message Container: " + e);
+        }
+    }
+
+    public synchronized Mail retrieve(String key) {
+        Mail mc = (Mail) or.get(key);
+        try {
+            mc.setMessage(sr.retrieve(key));
+        } catch (Exception me) {
+            throw new RuntimeException("Exception while retrieving mail: " + me.getMessage());
+        }
+        return mc;
+    }
+    
+    public synchronized void remove(Mail mail) {
+        remove(mail.getName());
+    }
+
+    public synchronized void remove(String key) {
+        lock(key);
+        sr.remove(key);
+        or.remove(key);
+        unlock(key);
+    }
+
+    public Enumeration list() {
+        return sr.list();
+    }
 }
 
     
