2f55f33a65e6db6d8889070234b1ea1395521414
Serge Knystautas <serge@apache.org>
2000-10-10 02:18:50 +0000

    Finished abstracting listserv and made Pico listserv the avalon implementation.  Still have Town implementation to come.
    git-svn-id: https://svn.apache.org/repos/asf/james/server@107182 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/org/apache/james/transport/mailets/GenericListserv.java
+++ trunk/src/org/apache/james/transport/mailets/GenericListserv.java
@@ -8,30 +8,24 @@
 
 package org.apache.james.transport.mailets;
 
+import java.io.*;
 import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
-import org.apache.avalon.*;
-import org.apache.james.*;
-import org.apache.james.usermanager.*;
-import org.apache.avalon.blocks.*;
 import org.apache.mailet.*;
-import org.apache.james.transport.*;
 
 /**
  * An abstract implementation of a listserv.  The underlying implementation must define
  * various settings, and can vary in their individual configuration.  Supports restricting
- * to members only, allowing attachments or not, and sending replies back to the list.
- *
- * This listserv will assume that when it processes a message, only this listserv is the
- * recipient.
+ * to members only, allowing attachments or not, sending replies back to the list, and an
+ * optional subject prefix.
  */
 public abstract class GenericListserv extends GenericMailet {
 
     /**
-     * Returns a Collection of String objects that are recipient's email addresses
+     * Returns a Collection of MailAddress objects of members to receive this email
      */
-    public abstract Collection getMembers();
+    public abstract Collection getMembers() throws ParseException;
 
     /**
      * Returns whether this list should restrict to senders only
@@ -51,7 +45,7 @@ public abstract class GenericListserv extends GenericMailet {
     /**
      * The email address that this listserv processes on
      */
-    public abstract String getListservAddress();
+    public abstract MailAddress getListservAddress() throws ParseException;
 
     /**
      * An optional subject prefix which will be surrounded by [].
@@ -61,66 +55,57 @@ public abstract class GenericListserv extends GenericMailet {
     /**
      * Processes the message.  Assumes it is the only recipient of this forked message.
      */
-    public final void service(Mail mail) {
-        //Do nothing
+    public final void service(Mail mail) throws MailetException, MessagingException {
+        try {
+            Collection members = new Vector();
+            members.addAll(getMembers());
+
+            //Check for members only flag....
+            if (isMembersOnly() && !members.contains(mail.getSender())) {
+                //Need to bounce the message to say they can't send to this list
+                getMailetContext().bounce(mail, "Only members of this listserv are allowed to send a message to this address.");
+                mail.setState(Mail.GHOST);
+                return;
+            }
+
+            //Check for no attachments
+            if (!isAttachmentsAllowed() && mail.getMessage().getContent() instanceof MimeMultipart) {
+                getMailetContext().bounce(mail, "You cannot send attachments to this listserv.");
+                mail.setState(Mail.GHOST);
+                return;
+            }
+
+            MimeMessage message = mail.getMessage();
+
+            //Set the subject if set
+            if (getSubjectPrefix() != null) {
+                String prefix = "[" + getSubjectPrefix() + "]";
+                String subj = message.getSubject();
+                //If the "prefix" is in the subject line, remove it and everything before it
+                int index = subj.indexOf(prefix);
+                if (index > -1) {
+                    if (index == 0) {
+                        subj = prefix + ' ' + subj.substring(index + prefix.length() + 1);
+                    } else {
+                        subj = prefix + ' ' + subj.substring(0, index) + subj.substring(index + prefix.length() + 1);
+                    }
+                } else {
+                    subj = prefix + ' ' + subj;
+                }
+
+                message.setSubject(subj);
+            }
+            if (isReplyToList()) {
+                message.setHeader("Reply-To", getListservAddress().toString());
+            }
+
+            //Send the message to the list members
+            getMailetContext().sendMail(getListservAddress(), members, message);
+
+            //Kill the old message
+            mail.setState(Mail.GHOST);
+        } catch (IOException ioe) {
+            throw new MailetException(ioe);
+        }
     }
-    /*
-    public final void service(Mail mail) throws Exception {
-        Collection members = getMembers();
-
-        //Check for recipients only....
-        if (isRecipientsOnly()) {
-            //later...
-        }
-
-        //Check for no attachments
-        if (!isAttachmentsAllowed()) {
-            //later...
-        }
-
-        //Do stuff
-        Vector newRcpts = new Vector ();
-
-        //Put all the other recipients into a new Vector of recipients
-        for (int j = 0; j < addr.length; j++)
-            if (j != i)
-                newRcpts.addElement (addr[j]);
-
-        //Add everyone on the listserv's recipient list
-        for (int j = 0; j < recipients.size (); j++)
-            if (!newRcpts.contains (recipients.elementAt (j)))
-                newRcpts.addElement (recipients.elementAt (j));
-
-        //Convert the vector into an array
-        addr = new InternetAddress[newRcpts.size ()];
-        for (int j = 0; j < newRcpts.size (); j++)
-            addr[j] = (InternetAddress)newRcpts.elementAt (j);
-
-        //Set the new recipient list
-        state.setRecipients (addr);
-
-        //Set the subject if so
-        if (subject != null)
-        {
-            String subj = message.getSubject ();
-            //If the "subject" is in the subject line, remove it and everything before it
-            int index = subj.indexOf (subject);
-            if (index > -1)
-            {
-                if (index == 0)
-                    subj = subject + ' ' + subj.substring (index + subject.length () + 1);
-                else
-                    subj = subject + ' ' + subj.substring (0, index) + subj.substring (index + subject.length () + 1);
-            } else
-                subj = subject + ' ' + subj;
-
-            message.setSubject (subj);
-        }
-        message.setHeader ("Reply-To", listservAddress.toString ());
-
-        state.setState (DeliveryState.PROCESSED);
-        return;
-    }
-    */
 }
-
