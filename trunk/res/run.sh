e8beb55f5b3356bf59d9483b6a73a4f14e62250c
Serge Knystautas <serge@apache.org>
2000-10-11 12:46:27 +0000

    Re-adding with unix linebreaks.
    git-svn-id: https://svn.apache.org/repos/asf/james/server@107196 13f79535-47bb-0310-9956-ffa450edef68

---
+++ trunk/res/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo
+echo "Avalon runner"
+echo
+
+if [ "$JAVA_HOME" = "" ] ; then
+  echo "ERROR: JAVA_HOME not found in your environment."
+  echo
+  echo "Please, set the JAVA_HOME variable in your environment to match the"
+  echo "location of the Java Virtual Machine you want to use."
+  exit 1
+fi
+
+LOCAL_CLASSPATH=Loader.jar:../lib/AvalonAware.jar:../lib/xerces_1_1_1.jar:../lib/mail_1_1_3.jar:../lib/activation.jar
+
+$JAVA_HOME/bin/java -cp $CLASSPATH:$LOCAL_CLASSPATH org.apache.avalon.loader.Main $*
