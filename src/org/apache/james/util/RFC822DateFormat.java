91e6f6ffcfe9bcf4cd09d073b62c20cefa0c0d59
Serge Knystautas <serge@apache.org>
2000-10-17 12:54:01 +0000

    Moved to new util package, and added InternetPrintWriter so that JAMES doesn't mess up SMTP communication on a unix server.
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@107235 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/james/util/RFC822DateFormat.java
@@ -0,0 +1,65 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included  with this distribution in *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+
+package org.apache.james.util;
+
+import java.util.*;
+import java.text.*;
+
+/**
+ * I suppose I could access some of the special messages within Sun's implementation of the JavaMail
+ * API, but I've always been told not to do that.  This class has one static method that takes a
+ * java.util.Date object and returns a nicely formatted String version of this, formatted as per the
+ * RFC822 mail date format.
+ * @author Serge Knystautas <sergek@lokitech.com>
+ * @version 0.9
+ */
+public class RFC822DateFormat {
+    private static DateFormat df;
+    private static DecimalFormat tz;
+
+    /**
+     * SimpleDateFormat will handle most of this for us, but the timezone won't match, so we do that
+     * manually
+     * @return java.lang.String
+     * @param d Date
+     */
+    public static String toString(Date d) {
+        if (df == null) {
+            df = new SimpleDateFormat("EE, d MMM yyyy HH:mm:ss");
+        }
+        if (tz == null) {
+            tz = new DecimalFormat("00");
+        }
+
+        StringBuffer sb = new StringBuffer(df.format(d));
+
+        sb.append(' ');
+
+        int min = TimeZone.getDefault().getRawOffset() / 1000 / 60;
+
+        if (min >= 0) {
+            sb.append('+');
+        } else {
+            sb.append('-');
+        }
+
+        min = Math.abs(min);
+
+        sb.append(tz.format(min / 60));
+        sb.append(tz.format(min % 60));
+
+        return sb.toString();
+    }
+
+}
+
+
+
+/*--- formatting done in "Sun Java Convention" style on 07-11-1999 ---*/
+
