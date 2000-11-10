31704fcc3884aeae0aa8b36ee824b8e3c7b68738
charles <dev-null@apache.org>
2000-11-10 15:47:15 +0000

    more ports...
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@107263 13f79535-47bb-0310-9956-ffa450edef68

---
+++ newavalon/src/org/apache/james/transport/matchers/RemoteAddrNotInNetwork.java
@@ -0,0 +1,59 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included  with this distribution in *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+
+package org.apache.james.transport.matchers;
+
+import org.apache.mailet.*;
+import java.net.*;
+import java.util.*;
+import javax.mail.*;
+
+/**
+ * Checks the network IP address of the sending server against a comma-
+ * delimited list of network addresses.
+ *
+ * @author  Serge Knystautas <sergek@lokitech.com>
+ */
+public class RemoteAddrNotInNetwork extends GenericMatcher {
+    private Collection networks = null;
+
+    public void init() throws MessagingException {
+        StringTokenizer st = new StringTokenizer(getCondition(), ", ", false);
+        networks = new Vector();
+        while (st.hasMoreTokens()) {
+            String addr = st.nextToken();
+            if (addr.equals("127.0.0.1")) {
+                //Add address of local machine as a match
+                try {
+                    InetAddress localaddr = InetAddress.getLocalHost();
+                    networks.add(localaddr.getHostAddress());
+                } catch (UnknownHostException uhe) {
+                }
+            }
+            if (addr.endsWith("*")) {
+                addr = addr.substring(0, addr.length() - 1);
+            }
+            networks.add(addr);
+        }
+    }
+
+    public Collection match(Mail mail) {
+        String host = mail.getRemoteAddr();
+        //Check to see whether it's in any of the networks... needs to be smarter to
+        // support subnets better
+        for (Iterator i = networks.iterator(); i.hasNext(); ) {
+            String invalidNetwork = i.next().toString();
+            if (host.startsWith(invalidNetwork)) {
+                //This is in this network... that's all we need for a failed match
+                return null;
+            }
+        }
+        //Could not match this to any network
+        return mail.getRecipients();
+    }
+}
