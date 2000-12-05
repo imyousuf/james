350a515f254b1d8445e152ebfad4c48fbb64ace6
Serge Knystautas <serge@apache.org>
2000-12-05 02:22:04 +0000

    Formatted the code to standard Java conventions.
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@107297 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/james/userrepository/UsersTownRepository.java
+++ src/org/apache/james/userrepository/UsersTownRepository.java
@@ -18,190 +18,181 @@ import com.workingdogs.town.*;
  */
 public class UsersTownRepository implements UsersRepository, Configurable {
 
-    private String name;
-    private String type;
-    private String model;
+	private String name;
+	private String type;
+	private String model;
 
-    private String destination;    
-    private String prefix;         
-    private String repositoryName; 
+	private String destination;
+	private String prefix;
+	private String repositoryName;
 
-    private String conndefinition;
-    private String tableName;
+	private String conndefinition;
+	private String tableName;
 
-    //  System defined logger funtion
-    private ComponentManager comp;
-    private Logger logger;
+	//  System defined logger funtion
+	private ComponentManager comp;
+	private Logger logger;
 
-    // Constructor - empty
-    public UsersTownRepository() {
-    }
+	// Constructor - empty
+	public UsersTownRepository() {
+	}
 
-    // Methods from interface Repository
-    public void setAttributes(String name, String destination, String type, String model) {
-        this.name = name;
-        this.model = model;
-        this.type = type;
+	// Methods from interface Repository
+	public void setAttributes(String name, String destination, String type, String model) {
+		this.name = name;
+		this.model = model;
+		this.type = type;
 
-        this.destination = destination;
-        int slash = destination.indexOf("//");
-        prefix = destination.substring(0, slash + 2);       
-        repositoryName = destination.substring(slash + 2);  
-     }
+		this.destination = destination;
+		int slash = destination.indexOf("//");
+		prefix = destination.substring(0, slash + 2);
+		repositoryName = destination.substring(slash + 2);
+	}
 
 
-    public void setComponentManager(ComponentManager comp) {
-       this.comp = comp;
-       // Store logger
-       this.logger = (Logger) comp.getComponent(Interfaces.LOGGER);
-    }
+	public void setComponentManager(ComponentManager comp) {
+		this.comp = comp;
+		// Store logger
+		this.logger = (Logger) comp.getComponent(Interfaces.LOGGER);
+	}
 
-   public void setConfiguration(Configuration conf) {
-       conndefinition = conf.getConfiguration("conn").getValue();
-       tableName = conf.getConfiguration("table").getValue("Users");
-    }
+	public void setConfiguration(Configuration conf) {
+		conndefinition = conf.getConfiguration("conn").getValue();
+		tableName = conf.getConfiguration("table").getValue("Users");
+	}
 
-    public String getName() {
-        return name; 
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getType() {
-        return type;
-    }
+	public String getType() {
+		return type;
+	}
 
-    public String getModel() {
-        return model;
-    }
+	public String getModel() {
+		return model;
+	}
 
-    public String getChildDestination(String childName) {
-        return prefix + repositoryName + "/" + childName;
-    }
+	public String getChildDestination(String childName) {
+		return prefix + repositoryName + "/" + childName;
+	}
 
-    public synchronized void addUser(String strUserName, Object attributes) {
-        try {
-          com.workingdogs.town.TableDataSet MRUser =
-            new com.workingdogs.town.TableDataSet(ConnDefinition.getInstance(conndefinition), tableName);
-          MRUser.setWhere("UserName = '" + strUserName+"'");
-          Record user = null;
-          if (MRUser.size() == 0) {
-              file://Add new user
-              user = MRUser.addRecord();
-              user.setValue("username", strUserName);
-              user.setValue("password", attributes.toString());
-              user.save();
-           } else {
-              file://User already exists: reject add 
-              logger.log("User "+strUserName+" already exists.",
-                 "UserManager", logger.WARNING);  // old Avalon logger format
-           }
-         } catch (Exception e) {
-           e.printStackTrace();
-           throw new RuntimeException("Exception caught while storing user: " + e);
-        }
-    }
+	public synchronized void addUser(String strUserName, Object attributes) {
+		try {
+			TableDataSet MRUser = new TableDataSet(ConnDefinition.getInstance(conndefinition), tableName);
+			MRUser.setWhere("UserName = '" + strUserName+"'");
+			Record user = null;
+			if (MRUser.size() == 0) {
+				// file://Add new user
+				user = MRUser.addRecord();
+				user.setValue("username", strUserName);
+				user.setValue("password", attributes.toString());
+				user.save();
+			} else {
+				// file://User already exists: reject add
+				logger.log("User "+strUserName+" already exists.",
+				"UserManager", logger.WARNING);  // old Avalon logger format
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("Exception caught while storing user: " + e);
+		}
+	}
 
-    public synchronized Object getAttributes(String strUserName) {
-        try {
-          com.workingdogs.town.TableDataSet MRUser =
-            new com.workingdogs.town.TableDataSet(ConnDefinition.getInstance(conndefinition), tableName);
-          MRUser.setWhere("UserName = '" + strUserName+"'");
-          if (MRUser.size() == 0) {
-            logger.log("User "+strUserName+" could not be found while fetching password.",
-               "UserManager", logger.WARNING);
-            return(null);
-          }
-          else {
-            Record user = MRUser.getRecord(0);
-            return ((Object) user.getAsString("Password"));
-          }
-        } catch (Exception e) {
-           throw new RuntimeException("Exception while retrieving password: " + e.getMessage());
-        }
-    }
+	public synchronized Object getAttributes(String strUserName) {
+		try {
+			TableDataSet MRUser = TableDataSet(ConnDefinition.getInstance(conndefinition), tableName);
+			MRUser.setWhere("UserName = '" + strUserName+"'");
+			if (MRUser.size() == 0) {
+				logger.log("User "+strUserName+" could not be found while fetching password.",
+				"UserManager", logger.WARNING);
+				return(null);
+			} else {
+				Record user = MRUser.getRecord(0);
+				return ((Object) user.getAsString("Password"));
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("Exception while retrieving password: " + e.getMessage());
+		}
+	}
 
-    public synchronized void removeUser(String strUserName) {
-        try {
-          com.workingdogs.town.TableDataSet MRUser = 
-             new com.workingdogs.town.TableDataSet(ConnDefinition.getInstance(conndefinition), tableName);
-         MRUser.setWhere("UserName = '" + strUserName + "'");
-          if (MRUser.size() == 0) {
-            file://User doesn't exists: reject delete
-            logger.log("User: " + strUserName + " does not exist.  Cannot delete", "UserManager", logger.WARNING);
-          }
-          else { 
-            Record user = MRUser.getRecord(0);
-            user.markToBeDeleted();
-            user.save();
-          }
-        } catch (Exception e) {
-           throw new RuntimeException("Exception while deleting user: " + e.getMessage());
-        }
-    }
+	public synchronized void removeUser(String strUserName) {
+		try {
+			TableDataSet MRUser = new TableDataSet(ConnDefinition.getInstance(conndefinition), tableName);
+			MRUser.setWhere("UserName = '" + strUserName + "'");
+			if (MRUser.size() == 0) {
+				// file://User doesn't exists: reject delete
+				logger.log("User: " + strUserName + " does not exist.  Cannot delete", "UserManager", logger.WARNING);
+			} else {
+				Record user = MRUser.getRecord(0);
+				user.markToBeDeleted();
+				user.save();
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("Exception while deleting user: " + e.getMessage());
+		}
+	}
 
-    public boolean contains(String strUserName) {
-        try {
-          com.workingdogs.town.TableDataSet MRUser = 
-            new com.workingdogs.town.TableDataSet(ConnDefinition.getInstance(conndefinition), tableName);
-          MRUser.setWhere("UserName = '" + strUserName + "'");
-         if (MRUser.size() > 0) {
-            return true;   // User exists
-          }
-          else {
-            return false;  // User does not exist
-          }
-        } catch (Exception e) {
-          throw new RuntimeException("Exception while retrieving user: " + e.getMessage());
-        }
-    }
+	public boolean contains(String strUserName) {
+		try {
+			TableDataSet MRUser = new TableDataSet(ConnDefinition.getInstance(conndefinition), tableName);
+			MRUser.setWhere("UserName = '" + strUserName + "'");
+			if (MRUser.size() > 0) {
+				return true;   // User exists
+			} else {
+				return false;  // User does not exist
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("Exception while retrieving user: " + e.getMessage());
+		}
+	}
 
-    public boolean test(String strUserName, Object attributes) {
-       try {
-          com.workingdogs.town.TableDataSet MRUser = 
-            new com.workingdogs.town.TableDataSet(ConnDefinition.getInstance(conndefinition), tableName);
-          MRUser.setWhere("UserName = '" + strUserName + "'");
-         if (MRUser.size() > 0) {
-            // UserName exists - check if the password is OK
-            Record user = MRUser.getRecord(0);
-            return(user.getAsString("Password").equals(attributes.toString()));
-          } else {
-             file://UserName does not exist
-             logger.log("User "+strUserName+" doesn't exist", "UserManager", logger.WARNING);
-             return(false);
-          }
-        } catch (Exception e) {
-          e.printStackTrace();
-          throw new RuntimeException("Exception caught while testing UserName: " + e.getMessage());
-        }
-    }
+	public boolean test(String strUserName, Object attributes) {
+		try {
+			TableDataSet MRUser = new TableDataSet(ConnDefinition.getInstance(conndefinition), tableName);
+			MRUser.setWhere("UserName = '" + strUserName + "'");
+			if (MRUser.size() > 0) {
+				// UserName exists - check if the password is OK
+				Record user = MRUser.getRecord(0);
+				return(user.getAsString("Password").equals(attributes.toString()));
+			} else {
+				// file://UserName does not exist
+				logger.log("User "+strUserName+" doesn't exist", "UserManager", logger.WARNING);
+				return(false);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("Exception caught while testing UserName: " + e.getMessage());
+		}
+	}
 
-    public int countUsers() {
-        try {
-          com.workingdogs.town.TableDataSet MRUser = 
-            new com.workingdogs.town.TableDataSet(ConnDefinition.getInstance(conndefinition), tableName);
-          MRUser.setWhere("UserName = *");
-          int nSize = MRUser.size();
-          return (int) nSize;
-         } catch (Exception e) {
-          e.printStackTrace();
-          throw new RuntimeException("Exception caught while testing UserName: " + e.getMessage());
-        }
-    }
+	public int countUsers() {
+	try {
+		TableDataSet MRUser = new TableDataSet(ConnDefinition.getInstance(conndefinition), tableName);
+		MRUser.setWhere("UserName = *");
+		int nSize = MRUser.size();
+		return (int) nSize;
+	} catch (Exception e) {
+		e.printStackTrace();
+		throw new RuntimeException("Exception caught while testing UserName: " + e.getMessage());
+	}
+	}
 
-    public Enumeration list() {
-       Vector list = new Vector();
+	public Enumeration list() {
+		Vector list = new Vector();
 
-      try {
-        QueryDataSet users = new QueryDataSet(ConnDefinition.getInstance(conndefinition),
-          "SELECT * FROM " + tableName);
-        for (int i = 0; i < users.size(); i++) {
-          list.add(users.getRecord(i).getAsString("UserName"));
-        }
-      } catch (Exception e) {
-        logger.log("Problem listing mailboxes. " + e ,"UserManager", logger.ERROR);
-        e.printStackTrace();
-	throw new RuntimeException("Exception while listing users: " + e.getMessage());
-      }
-      return list.elements();
-    }
+		try {
+			QueryDataSet users = new QueryDataSet(ConnDefinition.getInstance(conndefinition),
+			"SELECT * FROM " + tableName);
+			for (int i = 0; i < users.size(); i++) {
+				list.add(users.getRecord(i).getAsString("UserName"));
+			}
+		} catch (Exception e) {
+			logger.log("Problem listing mailboxes. " + e ,"UserManager", logger.ERROR);
+			e.printStackTrace();
+			throw new RuntimeException("Exception while listing users: " + e.getMessage());
+		}
+		return list.elements();
+	}
 
 }
