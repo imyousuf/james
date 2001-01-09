08d4d5aee8f957f373d394778395052a20f89f6b
charles <dev-null@apache.org>
2001-01-09 13:04:51 +0000

    ..moving..
    git-svn-id: https://svn.apache.org/repos/asf/james/server@107342 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/res/run.sh
+++ trunk/res/run.sh
@@ -1,7 +1,4 @@
-#!/bin/sh
-echo
-echo "Avalon runner"
-echo
+#! /bin/sh
 
 if [ "$JAVA_HOME" = "" ] ; then
   echo "ERROR: JAVA_HOME not found in your environment."
@@ -10,17 +7,6 @@ if [ "$JAVA_HOME" = "" ] ; then
   echo "location of the Java Virtual Machine you want to use."
   exit 1
 fi
+JAMES_CLASSPATH=avalon-loader.jar:../lib/xerces.jar:../lib/mail_1_2.jar:../lib/activation.jar:../lib/dnsjava.jar:../lib/town.jar
 
-if [ ! -f Loader.jar ] ; then
-  echo "ERROR: Loader.jar not found."
-  echo
-  echo "Please execute this 'run.sh' script from within the bin directory"
-  echo "by moving to the 'bin' directory and executing './run.sh'."
-  echo "If you obtained the source from CVS, build with the 'dist' target"
-  echo "then copy the JAMES zip or tar file to a working directory and unbundle it."
-  exit 1
-fi
-
-LOCAL_CLASSPATH=Loader.jar:../lib/AvalonAware.jar:../lib/xerces.jar:../lib/mail_1_2.jar:../lib/activation.jar
-
-$JAVA_HOME/bin/java -cp $CLASSPATH:$LOCAL_CLASSPATH org.apache.avalon.loader.Main $*
+$JAVA_HOME/bin/java -cp $CLASSPATH:$JAMES_CLASSPATH $AVALON_JVM_FLAGS AvalonLoader $*
