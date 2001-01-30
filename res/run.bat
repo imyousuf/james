806751a90dbaf648068ae90b7a00fd4b4aefacc9
Serge Knystautas <serge@apache.org>
2001-01-30 00:55:49 +0000

    Updated to changes in run.sh
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@107360 13f79535-47bb-0310-9956-ffa450edef68

--- res/run.bat
+++ res/run.bat
@@ -1,3 +1,4 @@
 @echo off
 
-java -jar avalon-loader.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
+set JAMES_CLASSPATH=avalon-loader.jar;../lib/xerces.jar;../lib/mail_1_2.jar;../lib/activation.jar;../lib/dnsjava.jar;../lib/town.jar
+java -cp %CLASSPATH%;%JAMES_CLASSPATH% %AVALON_JVM_FLAGS% AvalonLoader %1 %2 %3 %4 %5 %6 %7 %8 %9
