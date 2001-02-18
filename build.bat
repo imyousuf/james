d5c84103b05ae8b0f9600d89b066d69942cf90e3
Peter Donald <donaldp@apache.org>
2001-02-18 10:26:15 +0000

    Updated main scripts to reflect change of where ant exists
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@107379 13f79535-47bb-0310-9956-ffa450edef68

--- build.bat
+++ build.bat
@@ -4,7 +4,8 @@ echo.
 echo James Build System
 echo -------------------
 
-set ANT_HOME=.
+set ANT_HOME=tools
+set CLASSPATH=lib\xerces.jar
 
 %ANT_HOME%\bin\ant.bat -emacs %1 %2 %3 %4 %5 %6 %7 %8
 goto cleanup
