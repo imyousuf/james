a6211cb573ca58dfe65a10e1813397e6cfb6cae8
Peter Donald <donaldp@apache.org>
2001-03-05 01:53:39 +0000

    Unglobbed imports.
    Added copyright notice.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@107387 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/james/mailrepository/FileMimeMessageInputStream.java
+++ src/org/apache/james/mailrepository/FileMimeMessageInputStream.java
@@ -1,15 +1,20 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE file.
+ */
 package org.apache.james.mailrepository;
 
-import java.io.*;
-
-import org.apache.james.core.*;
-
-import org.apache.avalon.*;
+import java.io.IOException;
+import java.io.InputStream;
 import org.apache.cornerstone.services.Store;
-//import org.apache.avalon.blocks.*;
+import org.apache.james.core.JamesMimeMessageInputStream;
 
+public class FileMimeMessageInputStream 
+    extends JamesMimeMessageInputStream {
 
-public class FileMimeMessageInputStream extends JamesMimeMessageInputStream {
     //Define how to get to the data
     Store.StreamRepository sr = null;
     String key = null;
