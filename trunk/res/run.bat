02621b407dbe461a03150ba3c9221a0236365ddf
Peter Donald <donaldp@apache.org>
2001-03-05 11:21:07 +0000

    Updated scripts to work with latest kernel once it was moved to phoenix CVS
    git-svn-id: https://svn.apache.org/repos/asf/james/server@107391 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/res/run.bat
+++ trunk/res/run.bat
@@ -1,4 +1,3 @@
 @echo off
 
-set JAMES_CLASSPATH=avalon-loader.jar;../lib/xerces.jar;../lib/mail_1_2.jar;../lib/activation.jar;../lib/dnsjava.jar;../lib/town.jar
-java -cp %CLASSPATH%;%JAMES_CLASSPATH% %AVALON_JVM_FLAGS% AvalonLoader %1 %2 %3 %4 %5 %6 %7 %8 %9
+java %PHOENIX_JVM_OPTS% -jar phoenix-loader.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
--- trunk/res/run.sh
+++ trunk/res/run.sh
@@ -7,6 +7,5 @@ if [ "$JAVA_HOME" = "" ] ; then
   echo "location of the Java Virtual Machine you want to use."
   exit 1
 fi
-JAMES_CLASSPATH=avalon-loader.jar:../lib/xerces.jar:../lib/mail_1_2.jar:../lib/activation.jar:../lib/dnsjava.jar:../lib/town.jar
 
-$JAVA_HOME/bin/java -cp $CLASSPATH:$JAMES_CLASSPATH $AVALON_JVM_FLAGS AvalonLoader $*
+$JAVA_HOME/bin/java $PHOENIX_JVM_OPTS -jar phoenix-loader.jar $*
