14fbda56ee80fc103d9c016c40954140da30fc61
Harmeet Bedi <hbedi@apache.org>
2001-04-10 16:02:12 +0000

    NNTP implementation 1st pass
    git-svn-id: https://svn.apache.org/repos/asf/james/server@107460 13f79535-47bb-0310-9956-ffa450edef68

---
+++ trunk/src/org/apache/james/nntpserver/NNTPServer.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE file.
+ */
+package org.apache.james.nntpserver;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import org.apache.avalon.Component;
+import org.apache.avalon.ComponentManager;
+import org.apache.avalon.ComponentManagerException;
+import org.apache.avalon.DefaultComponentManager;
+import org.apache.avalon.Initializable;
+import org.apache.avalon.configuration.Configuration;
+import org.apache.avalon.configuration.ConfigurationException;
+import org.apache.cornerstone.services.connection.AbstractService;
+import org.apache.cornerstone.services.connection.ConnectionHandlerFactory;
+import org.apache.cornerstone.services.connection.DefaultHandlerFactory;
+import org.apache.james.nntpserver.repository.NNTPRepository;
+import org.apache.james.nntpserver.repository.NNTPUtil;
+
+/**
+ * @author Harmeet <hbedi@apache.org>
+ */
+public class NNTPServer extends AbstractService {
+
+    private Component repository;
+    protected ConnectionHandlerFactory createFactory() {
+        return new DefaultHandlerFactory( NNTPHandler.class );
+    }
+
+    public void configure( final Configuration configuration )
+        throws ConfigurationException 
+    {
+        //System.out.println(getClass().getName()+": configure");
+        m_port = configuration.getChild( "port" ).getValueAsInt( 119 );
+
+        try { 
+            String bindAddress = configuration.getChild( "bind" ).getValue( null );
+            if( null != bindAddress ) 
+                m_bindTo = InetAddress.getByName( bindAddress ); 
+        } catch( final UnknownHostException unhe ) {
+            throw new ConfigurationException( "Malformed bind parameter", unhe );
+        }
+
+        final String useTLS = configuration.getChild("useTLS").getValue( "" );
+        if( useTLS.equals( "TRUE" ) ) 
+            m_serverSocketType = "ssl";
+
+        repository = (Component)NNTPUtil.createInstance
+            (configuration.getChild("repository"),getLogger(),
+             "org.apache.james.nntpserver.repository.NNTPRepositoryImpl");
+
+        super.configure( configuration.getChild( "nntphandler" ) );
+        getLogger().info("configured NNTPServer to run at : "+m_port);
+    }
+
+    public void compose( final ComponentManager componentManager )
+        throws ComponentManagerException
+    {
+        //System.out.println(getClass().getName()+": compose");
+        DefaultComponentManager mgr = new DefaultComponentManager(componentManager);
+        mgr.put("org.apache.james.nntpserver.repository.NNTPRepository",repository);
+        super.compose(mgr);
+    }
+
+    public void init() throws Exception {
+        //System.out.println(getClass().getName()+": init");
+        super.init();
+        if ( repository instanceof Initializable )
+            ((Initializable)repository).init();
+    }
+}
---
+++ trunk/src/org/apache/james/nntpserver/repository/NNTPUtil.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE file.
+ */
+package org.apache.james.nntpserver.repository;
+
+import java.io.*;
+import org.apache.avalon.Initializable;
+import org.apache.avalon.Loggable;
+import org.apache.log.Logger;
+import org.apache.avalon.configuration.Configurable;
+import org.apache.avalon.configuration.Configuration;
+import org.apache.avalon.configuration.ConfigurationException;
+import org.apache.james.nntpserver.NNTPException;
+
+// processes entries and sends to appropriate groups.
+// eats up inappropriate entries.
+public class NNTPUtil {
+    static File getDirectory(Configuration configuration,String child) 
+        throws ConfigurationException 
+    {
+        String str = configuration.getChild(child).getValue();
+        File f = new File(str);
+        if ( f.exists() && f.isFile() )
+            throw new NNTPException("Expecting '"+f.getAbsolutePath()+"' directory");
+        if ( f.exists() == false )
+            f.mkdirs();
+        return f;
+    }
+    public static Object createInstance(Configuration configuration,Logger logger,
+                                        String clsName) throws ConfigurationException 
+    {
+        try { clsName = configuration.getAttribute("class");
+        } catch(ConfigurationException ce) { }
+        try {
+            Object obj = Class.forName(clsName).newInstance();
+            if ( obj instanceof Loggable )
+            ((Loggable)obj).setLogger( logger );
+            if ( obj instanceof Configurable )
+                ((Configurable)obj).configure(configuration.getChild("configuration"));
+            return obj;
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            throw new ConfigurationException("spooler initialization failed",ex);
+        }
+    }
+
+    public static void show(Configuration conf,PrintStream prt) {
+        prt.println("conf.getClass="+conf.getClass().getName());
+        prt.println("name="+conf.getName());
+        Configuration[] children = conf.getChildren();
+        for ( int i = 0 ; i < children.length ; i++ )
+            prt.println(i+". "+children[i].getName());
+    }
+}
