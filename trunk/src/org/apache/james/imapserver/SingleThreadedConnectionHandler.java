9843f5dc4fe15ced38ec67e783ca0d319585fdc0
Harmeet Bedi <hbedi@apache.org>
2001-04-21 05:49:12 +0000

    Split code into different blocks.
    git-svn-id: https://svn.apache.org/repos/asf/james/server@107473 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/org/apache/james/imapserver/SingleThreadedConnectionHandler.java
+++ trunk/src/org/apache/james/imapserver/SingleThreadedConnectionHandler.java
@@ -148,6 +148,8 @@ public class SingleThreadedConnectionHandler
 
     public void contextualize( final Context context ) {
         servername = (String)context.get( Constants.HELO_NAME );
+        if ( servername == null )
+            servername = "IMAPServer";
     }
 
     public void configure( final Configuration configuration )
