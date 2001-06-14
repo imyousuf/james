c25e10569892c69deb3f393ba6208a981377f5d9
Charles R. Benett <charlesb@apache.org>
2001-06-14 13:00:44 +0000

    Tidy up code - ongoing campaign
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@107551 13f79535-47bb-0310-9956-ffa450edef68

--- proposals/v1.3/java/org/apache/james/util/Base64.java
+++ proposals/v1.3/java/org/apache/james/util/Base64.java
@@ -12,12 +12,19 @@ import javax.mail.internet.*;
 
 /**
  * Simple Base64 string decoding function
- * @author Jason Borden <jborden@javasense.com>  
+ * @author Jason Borden <jborden@javasense.com>
+ *
+ * This is $Revision: 1.2 $
+ * Committed on $Date: 2001/06/14 13:00:44 $ by: $Author: charlesb $ 
  */
 
 public class Base64 {
 
     public static BufferedReader decode(String b64string) throws Exception {
-        return new BufferedReader(new InputStreamReader(MimeUtility.decode(new ByteArrayInputStream(b64string.getBytes()), "base64")));
+        return new BufferedReader(
+                   new InputStreamReader(
+                       MimeUtility.decode(
+                            new ByteArrayInputStream(
+                                b64string.getBytes()), "base64")));
     }
 }
