0e9b24d2bba862185cafbc71af53e5362d627aa6
Serge Knystautas <serge@apache.org>
2001-08-11 21:21:35 +0000

    Modified script to support a null sender to support MAIL FROM: <>
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@107608 13f79535-47bb-0310-9956-ffa450edef68

--- src/sql/james-message-sqlserver.sql
+++ src/sql/james-message-sqlserver.sql
@@ -4,7 +4,7 @@ CREATE TABLE [dbo].[Message] (
 	[repository_name] [varchar] (200) NOT NULL ,
 	[message_state] [varchar] (30) NOT NULL ,
 	[error_message] [varchar] (200) NULL ,
-	[sender] [varchar] (100) NOT NULL ,
+	[sender] [varchar] (100) NULL ,
 	[recipients] [text] NOT NULL ,
 	[remote_host] [varchar] (100) NOT NULL ,
 	[remote_addr] [varchar] (20) NOT NULL ,
