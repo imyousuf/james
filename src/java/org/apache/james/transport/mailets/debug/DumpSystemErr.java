559c08ca10f2cfbcfce10dde04b5607f634b8149
Serge Knystautas <serge@apache.org>
2001-08-11 21:32:10 +0000

    Moving development-time mailets to another package (and added DumpSystemErr mailet)
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@107616 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/james/transport/mailets/debug/DumpSystemErr.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE file.
+ */
+package org.apache.james.transport.mailets.debug;
+
+import java.io.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import org.apache.mailet.*;
+
+/**
+ * Debugging purpose Mailet.  Sends the message to System.err
+ *
+ * @author  Serge Knystautas <sergek@lokitech.com>
+ */
+public class DumpSystemErr extends GenericMailet {
+
+    public void service(Mail mail) throws MessagingException {
+        try {
+            MimeMessage message = mail.getMessage();
+            message.writeTo(System.err);
+        } catch (IOException ioe) {
+            log("error printing message", ioe);
+        }
+    }
+
+    public String getMailetInfo() {
+        return "Dumps message to System.err";
+    }
+}
