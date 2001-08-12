b227985e8efb61db3562c9bfcaad3540a65e89d2
Serge Knystautas <serge@apache.org>
2001-08-12 06:14:56 +0000

    Supports multipart messages now (alternative and attachments)
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@107623 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/james/transport/mailets/AddFooter.java
+++ src/java/org/apache/james/transport/mailets/AddFooter.java
@@ -38,14 +38,27 @@ public class AddFooter extends GenericMailet {
             MimeMessage message = mail.getMessage();
 
             //I want to modify the right message body
-            String contentType = message.getContentType();
-            if (contentType == null || contentType.startsWith("text/plain")) {
+            if (message.isMimeType("text/plain")) {
                 //This is a straight text message... just append the single part normally
-                String content = message.getContent().toString();
-                content += getFooterText();
-                message.setText(content);
+                addToText(message);
+            } else if (message.isMimeType("multipart/mixed")) {
+                //Find the first body part, and determine what to do then.
+                MimeMultipart multipart = (MimeMultipart)message.getContent();
+                MimeBodyPart part = (MimeBodyPart)multipart.getBodyPart(0);
+                attachFooter(part);
+                //We have to do this because of a bug in JavaMail (ref id 4404733)
+                message.setContent(multipart);
+            } else if (message.isMimeType("multipart/alternative")) {
+                //Find the HTML and text message types and add to each
+                MimeMultipart multipart = (MimeMultipart)message.getContent();
+                for (int i = 0; i < multipart.getCount(); i++) {
+                    MimeBodyPart part = (MimeBodyPart)multipart.getBodyPart(i);
+                    attachFooter(part);
+                }
+                //We have to do this because of a bug in JavaMail (ref id 4404733)
+                message.setContent(multipart);
             } else {
-                //System.err.println(message.getContentType());
+                //Give up... we won't attach the footer to this message
             }
         } catch (IOException ioe) {
             throw new MessagingException("Could not read message", ioe);
@@ -60,7 +73,56 @@ public class AddFooter extends GenericMailet {
         return text;
     }
 
+    /**
+     * This is exposed as a method for easy subclassing to provide alternate ways
+     * to get the footer text.  By default, this will take the footer text,
+     * converting the linefeeds to &lt;br&gt; tags.
+     */
+    public String getFooterHTML() {
+        String text = getFooterText();
+        StringBuffer sb = new StringBuffer();
+        StringTokenizer st = new StringTokenizer(text, "\r\n", true);
+        while (st.hasMoreTokens()) {
+            String token = st.nextToken();
+            if (token.equals("\r")) {
+                continue;
+            }
+            if (token.equals("\n")) {
+                sb.append("<br />\n");
+            } else {
+                sb.append(token);
+            }
+        }
+        return sb.toString();
+    }
+
     public String getMailetInfo() {
         return "AddFooter Mailet";
+    }
+
+    protected void addToText(MimePart part) throws MessagingException, IOException {
+        String content = part.getContent().toString();
+        if (!content.endsWith("\n")) {
+            content += "\r\n";
+        }
+        content += getFooterText();
+        part.setText(content);
+    }
+
+    protected void addToHTML(MimePart part) throws MessagingException, IOException {
+        String content = part.getContent().toString();
+        content += "<br>" + getFooterHTML();
+        part.setContent(content, part.getContentType());
+    }
+
+    protected void attachFooter(MimePart part) throws MessagingException, IOException {
+        if (part.isMimeType("text/plain")) {
+            addToText(part);
+        } else if (part.isMimeType("text/html")) {
+            addToHTML(part);
+        } else {
+            System.err.println(part.getContentType());
+        }
+        //Give up... we won't attach the footer to this message
     }
 }
