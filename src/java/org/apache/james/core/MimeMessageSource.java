c11c634ceeee875763430118612a9e1ce80d62a9
Serge Knystautas <serge@apache.org>
2001-10-09 04:00:17 +0000

    Add small documentation and license header.
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@107685 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/james/core/MimeMessageSource.java
+++ src/java/org/apache/james/core/MimeMessageSource.java
@@ -1,8 +1,18 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included  with this distribution in
+ * the LICENSE file.
+ */
 package org.apache.james.core;
 
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * A source of a MimeMessage.
+ */
 public abstract class MimeMessageSource {
 	/**
 	 * Return an input stream to the data
--- src/java/org/apache/james/core/MimeMessageWrapper.java
+++ src/java/org/apache/james/core/MimeMessageWrapper.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included  with this distribution in
+ * the LICENSE file.
+ */
 package org.apache.james.core;
 
 import java.io.*;
@@ -7,6 +14,11 @@ import javax.activation.*;
 import javax.mail.*;
 import javax.mail.internet.*;
 
+/**
+ * This object wraps a MimeMessage, only loading the underlying MimeMessage
+ * object when needed.  Also tracks if changes were made to reduce
+ * unnecessary saves.
+ */
 public class MimeMessageWrapper extends MimeMessage {
 
     /**
