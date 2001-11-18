8d2ccf5a584dad8e4fc3085e86b417264033a4c7
Harmeet Bedi <hbedi@apache.org>
2001-11-18 01:00:37 +0000

    bugfix: Implementation did not comply with 7.1.1: Initial connection, if message posting is not allowed 	separated authentication and authorization checks.
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@107743 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/james/nntpserver/AuthServiceImpl.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE file.
+ */
+package org.apache.james.nntpserver;
+
+import org.apache.avalon.framework.component.ComponentException;
+import org.apache.avalon.framework.component.ComponentManager;
+import org.apache.avalon.framework.component.Composable;
+import org.apache.avalon.framework.configuration.Configurable;
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.avalon.framework.logger.AbstractLoggable;
+import org.apache.avalon.phoenix.Block;
+import org.apache.james.services.UsersRepository;
+import org.apache.james.services.UsersStore;
+
+/**
+ * Provides Authentication State. 
+ * Authenticates users.
+ * Access Control for User Commands.
+ *
+ * Implementation is NNTP Specific. 
+ * Would it be better to have a James Authentication Service ?
+ *
+ * Application could define and plugin their own authentication service.
+ *
+ * @author  Harmeet <harmeet@kodemuse.com> 
+ */
+public class AuthServiceImpl extends AbstractLoggable 
+    implements AuthService, Composable, Configurable, Block 
+{
+    protected boolean authRequired;
+    protected UsersRepository repo;
+    protected String user;
+    protected String password;
+    protected boolean userSet = false;
+    protected boolean passwordSet = false;
+
+    public boolean isAuthorized(String command) {
+        boolean allowed = isAuthenticated();
+        // some commads are authorized, even if the user is not authenticated
+        allowed = allowed || command.equalsIgnoreCase("AUTHINFO");
+        allowed = allowed || command.equalsIgnoreCase("AUTHINFO");
+        allowed = allowed || command.equalsIgnoreCase("MODE");
+        allowed = allowed || command.equalsIgnoreCase("QUIT");
+        return allowed;
+    }
+
+    public void compose( final ComponentManager componentManager )
+        throws ComponentException
+    {
+        UsersStore usersStore = (UsersStore)componentManager.lookup(UsersStore.ROLE);
+        repo = usersStore.getRepository("LocalUsers");
+    }
+
+    public void configure( Configuration configuration ) throws ConfigurationException {
+        authRequired =
+            configuration.getChild("authRequired").getValueAsBoolean(false);
+        getLogger().debug("Auth required state is :" + authRequired);
+    }
+
+    public boolean isAuthenticated() {
+        if ( authRequired ) {
+            if  (userSet && passwordSet ) {
+                return repo.test(user,password);
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    public void setUser(String user) {
+        if (user != null && user.trim().length() != 0) {
+            this.user = user;
+            userSet = true;
+        }
+        this.password = null;
+    }
+
+    public void setPassword(String password) {
+        if (password != null && password.trim().length() != 0) {
+            this.password = password;
+            passwordSet = true;
+        }
+    }
+}
