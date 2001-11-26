a69a0a641d58c2ea78fdf46c9d2ca6f1e1d72272
Serge Knystautas <serge@apache.org>
2001-11-26 03:28:25 +0000

    Added a read(byte[], off, len) implementation.
    git-svn-id: https://svn.apache.org/repos/asf/james/server@107754 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/java/org/apache/james/smtpserver/SizeLimitedInputStream.java
+++ trunk/src/java/org/apache/james/smtpserver/SizeLimitedInputStream.java
@@ -16,10 +16,12 @@ import org.apache.james.smtpserver.*;
   * @author Matthew Pangaro <mattp@lokitech.com>
   */
 public class SizeLimitedInputStream extends InputStream {
-    /** Maximum number of bytes to read.
+    /**
+     * Maximum number of bytes to read.
      */
     private long maxmessagesize = 0;
-    /** Running total of bytes read from wrapped stream.
+    /**
+     * Running total of bytes read from wrapped stream.
      */
     private long bytesread = 0;
 
@@ -27,7 +29,8 @@ public class SizeLimitedInputStream extends InputStream {
      */
     private InputStream in = null;
 
-    /** Constructor for the stream. Wraps an underlying stream.
+    /**
+     * Constructor for the stream. Wraps an underlying stream.
      * @param in InputStream to use as basis for new Stream.
      * @param maxmessagesize Message size limit, in Kilobytes
      */
@@ -36,7 +39,26 @@ public class SizeLimitedInputStream extends InputStream {
         this.maxmessagesize = maxmessagesize;
     }
 
-    /** Overrides the read method of InputStream to call the read() method of the
+    /**
+     * Overrides the read method of InputStream to call the read() method of the
+     * wrapped input stream.
+     * @throws IOException Throws a MessageSizeException, which is a sub-type of IOException
+     * @return Returns the number of bytes read.
+     */
+    public int read(byte[] b, int off, int len) throws IOException {
+        int l = in.read(b, off, len);
+
+        bytesread += l;
+
+        if (maxmessagesize > 0 && bytesread > maxmessagesize) {
+            throw new MessageSizeException();
+        }
+
+        return l;
+    }
+
+    /**
+     * Overrides the read method of InputStream to call the read() method of the
      * wrapped input stream.
      * @throws IOException Throws a MessageSizeException, which is a sub-type of IOException.
      * @return Returns the int character value of the byte read.
@@ -50,4 +72,3 @@ public class SizeLimitedInputStream extends InputStream {
         }
     }
 }
-
