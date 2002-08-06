4bfe89aea0a4e65b70274664fdc641b4fcae01f5
Peter M. Goldstein <pgoldstein@apache.org>
2002-08-06 03:01:57 +0000

    An enhancement to the RemoteAddrNotInNetwork and RemoteAddrInNetwork matchers that allows them to take host names as well as IP addresses as parameter.
    Thanks to Noel Bergman for this submission.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/james/server@107955 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/java/org/apache/james/transport/matchers/RemoteAddrInNetwork.java
+++ trunk/src/java/org/apache/james/transport/matchers/RemoteAddrInNetwork.java
@@ -20,13 +20,13 @@ import java.util.Vector;
 
 /**
  * Checks the IP address of the sending server against a comma-
- * delimited list of  IP addresses.
+ * delimited list of IP addresses or domain names.
  * <P>Networks should be indicated with a wildcard *, e.g. 192.168.* 
  * <br>Note: The wildcard can go at any level, the matcher will match if the
  * sending host's IP address (as a String based on the octet representation)
  * starts with the String indicated in the configuration file, excluding the
  * wildcard.
- * <p>Multiple addresses can be indicated, e.g: '127.0.0.1,192.168.*'
+ * <p>Multiple addresses can be indicated, e.g: '127.0.0.1,192.168.*,domain.tld'
  *
  * @author  Serge Knystautas <sergek@lokitech.com>
  */
@@ -46,10 +46,18 @@ public class RemoteAddrInNetwork extends GenericMatcher {
                 } catch (UnknownHostException uhe) {
                 }
             }
-            if (addr.endsWith("*")) {
-                addr = addr.substring(0, addr.length() - 1);
+
+            try {
+                if (addr.endsWith("*")) {
+                    addr = addr.substring(0, addr.length() - 1);
+                }
+                else {
+                    addr = InetAddress.getByName(addr).getHostAddress();
+                }
+                networks.add(addr);
+            } catch (UnknownHostException uhe) {
+                log("Cannot match against invalid domain: " + uhe.getMessage());
             }
-            networks.add(addr);
         }
     }
 
