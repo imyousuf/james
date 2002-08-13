9438336a63a392c6fa3fea6b8bf6e4927a689b44
Peter M. Goldstein <pgoldstein@apache.org>
2002-08-13 19:29:58 +0000

    This patch fixes build issues that arose with the move to the latest Phoenix.
    Thanks to Noel Bergman for the patch
    
    
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@107980 13f79535-47bb-0310-9956-ffa450edef68

--- build.bat
+++ build.bat
@@ -4,11 +4,15 @@ echo.
 echo James Build System
 echo -------------------
 
+set OLD_ANT_HOME=%ANT_HOME%
 set ANT_HOME=tools
-set CLASSPATH=lib\xerces-1.4.3.jar;tools\lib\velocity-1.3-dev.jar;tools\lib\jdom-b7.jar
+
+set OLD_CLASSPATH=%CLASSPATH%
+set CLASSPATH=phoenix-bin\lib\xerces-2.0.1.jar;phoenix-bin\lib\xml-apis.jar;tools\lib\velocity-1.3-dev.jar;tools\lib\jdom-b7.jar
 
 %ANT_HOME%\bin\ant.bat -emacs %1 %2 %3 %4 %5 %6 %7 %8
 goto cleanup
 
 :cleanup
-set ANT_HOME=
+set ANT_HOME=%OLD_ANT_HOME%
+set CLASSPATH=%OLD_CLASSPATH%
