0e009e456e6e629956a352d56d24452f6a9f8e0a
Peter M. Goldstein <pgoldstein@apache.org>
2002-08-24 18:45:28 +0000

    Updated comments to correct a minor oversight.
    Thanks to Noel Bergman
    
    
    git-svn-id: https://svn.apache.org/repos/asf/james/server@108020 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/java/org/apache/james/transport/matchers/RemoteAddrNotInNetwork.java
+++ trunk/src/java/org/apache/james/transport/matchers/RemoteAddrNotInNetwork.java
@@ -20,13 +20,13 @@ import java.util.Vector;
 
 /**
  * Checks the IP address of the sending server against a comma-
- * delimited list of IP addresses.
+ * delimited list of IP addresses or domain names.
  * <P>Networks should be indicated with a wildcard *, e.g. 192.168.*
  * <br>Note: The wildcard can go at any level, the matcher will match if the
  * sending host's IP address (as a String based on the octet representation)
  * starts with the String indicated in the configuration file, excluding the
  * wildcard.
- * <p>Multiple addresses can be indicated, e.g: '127.0.0.1,192.168.*'
+ * <p>Multiple addresses can be indicated, e.g: '127.0.0.1,192.168.*,domain.tld'
  *
  * @author  Serge Knystautas <sergek@lokitech.com>
  */
