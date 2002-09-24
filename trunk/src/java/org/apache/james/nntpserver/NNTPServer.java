f96861aafc6534350ba641f9b9f82d03e6bcc04d
Danny Angus <danny@apache.org>
2002-09-24 15:33:49 +0000

    Added an enabled="true|false" attribute to block conf for these blocks to easily turn listening on and off without having to fully remove the block.
    git-svn-id: https://svn.apache.org/repos/asf/james/server@108075 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/java/org/apache/james/nntpserver/NNTPServer.java
+++ trunk/src/java/org/apache/james/nntpserver/NNTPServer.java
@@ -6,60 +6,53 @@
  * the LICENSE file.
  */
 package org.apache.james.nntpserver;
-
 import org.apache.avalon.cornerstone.services.connection.AbstractService;
 import org.apache.avalon.cornerstone.services.connection.ConnectionHandlerFactory;
 import org.apache.avalon.cornerstone.services.connection.DefaultHandlerFactory;
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.ConfigurationException;
 import org.apache.avalon.framework.component.Component;
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-
 /**
  * NNTP Server Protocol Handler
  *
  * @author Harmeet Bedi <harmeet@kodemuse.com>
+ * @author  <a href="mailto:danny@apache.org">Danny Angus</a>
  */
 public class NNTPServer extends AbstractService implements Component {
-
+    private boolean enabled = true;
     protected ConnectionHandlerFactory createFactory() {
-        return new DefaultHandlerFactory( NNTPHandler.class );
+        return new DefaultHandlerFactory(NNTPHandler.class);
     }
-
     /**
      * Pass the <code>Configuration</code> to the instance.
      *
      * @param configuration the class configurations.
      * @throws ConfigurationException if an error occurs
      */
-    public void configure( final Configuration configuration )
-        throws ConfigurationException
-    {
-        //System.out.println(getClass().getName() + ": configure");
-        m_port = configuration.getChild( "port" ).getValueAsInteger( 119 );
-
-        try {
-            String bindAddress = configuration.getChild( "bind" ).getValue( null );
-            if( null != bindAddress )
-                m_bindTo = InetAddress.getByName( bindAddress );
-        } catch( final UnknownHostException unhe ) {
-            throw new ConfigurationException( "Malformed bind parameter", unhe );
-        }
-
-        final boolean useTLS = configuration.getChild("userTLS").getValueAsBoolean( false );
-        if ( useTLS )
-        {
-            m_serverSocketType = "ssl";
-        }
-
-        super.configure( configuration.getChild( "handler" ) );
-        if (getLogger().isInfoEnabled()) {
-            getLogger().info("configured NNTPServer to run at : " + m_port);
+    public void configure(final Configuration configuration) throws ConfigurationException {
+        if (configuration.getAttribute("enabled").equalsIgnoreCase("false")) {
+            enabled = false;
+        } else {
+            m_port = configuration.getChild("port").getValueAsInteger(119);
+            try {
+                String bindAddress = configuration.getChild("bind").getValue(null);
+                if (null != bindAddress)
+                    m_bindTo = InetAddress.getByName(bindAddress);
+            } catch (final UnknownHostException unhe) {
+                throw new ConfigurationException("Malformed bind parameter", unhe);
+            }
+            final boolean useTLS = configuration.getChild("userTLS").getValueAsBoolean(false);
+            if (useTLS) {
+                m_serverSocketType = "ssl";
+            }
+            super.configure(configuration.getChild("handler"));
+            if (getLogger().isInfoEnabled()) {
+                getLogger().info("configured NNTPServer to run at : " + m_port);
+            }
         }
     }
-
     /**
      * Initialize the component. Initialization includes
      * allocating any resources required throughout the
@@ -68,8 +61,17 @@ public class NNTPServer extends AbstractService implements Component {
      * @throws Exception if an error occurs
      */
     public void initialize() throws Exception {
-        //System.out.println(getClass().getName() + ": init");
-        super.initialize();
-        System.out.println("Started NNTP Server "+m_connectionName);
+        if (enabled) {
+            super.initialize();
+            System.out.println("NNTP Server Started " + m_connectionName);
+        } else {
+            getLogger().info("NNTP Server Disabled");
+            System.out.println("NNTP Server Disabled");
+        }
     }
+    public void dispose() {
+        if (enabled) {
+            super.dispose();
+        }
+    }    
 }
