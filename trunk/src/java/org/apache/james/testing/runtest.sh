65241454b512e465eefae2140a05231d5b2da119
Peter M. Goldstein <pgoldstein@apache.org>
2002-10-12 21:22:34 +0000

    Removing patch that was not submitted for review, introduced inappropriate files into the repository, and violated James stated coding standards.
    git-svn-id: https://svn.apache.org/repos/asf/james/server@108140 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/java/org/apache/james/testing/runtest.sh
+++
@@ -1,3 +0,0 @@
-echo 'runs james test suite and outputs result in csv file format'
-echo 'test, method, time taken, successful invocations, invocation attempt'
-java org.apache.james.testing.Main testconf.xml | grep "stat: " | sed "s/.*stat: //g"
