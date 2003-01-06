e872e94e6835b93d6b2fe12435e4c80b21f44038
Danny Angus <danny@apache.org>
2003-01-06 15:56:24 +0000

    removing avalon dependance in mailets. Step six remove avalon dependance from standard mailets
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@108308 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/james/transport/mailets/AvalonListserv.java
+++ src/java/org/apache/james/transport/mailets/AvalonListserv.java
@@ -7,13 +7,14 @@
  */
 package org.apache.james.transport.mailets;
 
-import org.apache.avalon.framework.component.ComponentException;
-import org.apache.avalon.framework.component.ComponentManager;
+//import org.apache.avalon.framework.component.ComponentException;
+//import org.apache.avalon.framework.component.ComponentManager;
 import org.apache.james.Constants;
-import org.apache.james.services.UsersRepository;
-import org.apache.james.services.UsersStore;
+//import org.apache.james.services.UsersStore;
 import org.apache.mailet.MailAddress;
+import org.apache.mailet.UsersRepository;
 
+import javax.mail.MessagingException;
 import javax.mail.internet.ParseException;
 import java.util.Collection;
 import java.util.Iterator;
@@ -52,7 +53,7 @@ import java.util.Vector;
  * "[PREFIX] ", if false, the prefix will be used literally.
  *
  * @author  <a href="sergek@lokitech.com">Serge Knystautas </a>
- * @version This is $Revision: 1.7 $
+ * @version This is $Revision: 1.8 $
  */
 public class AvalonListserv extends GenericListserv {
 
@@ -114,17 +115,13 @@ public class AvalonListserv extends GenericListserv {
             // Ignore any exceptions, default to true
         }
 
-        ComponentManager compMgr = (ComponentManager)getMailetContext().getAttribute(Constants.AVALON_COMPONENT_MANAGER);
-        try {
-            UsersStore usersStore = (UsersStore)compMgr.lookup("org.apache.james.services.UsersStore");
-            String repName = getInitParameter("repositoryName");
 
-            members = (UsersRepository)usersStore.getRepository( repName );
-        } catch (ComponentException cnfe) {
-            log("Failed to retrieve Store component:" + cnfe.getMessage());
-        } catch (Exception e) {
-            log("Failed to retrieve Store component:" + e.getMessage());
-        }
+            try {
+                members = getMailetContext().getUserRepository(getInitParameter("repositoryName"));
+            } catch (MessagingException e) {
+                log("init failed cannot access users repository "+getInitParameter("repositoryName"), e);
+            }
+
     }
 
     public Collection getMembers() throws ParseException {
