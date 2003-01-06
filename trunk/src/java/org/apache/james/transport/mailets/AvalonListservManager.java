1656bd5ffd3a3761c736370708da986c2476e225
Danny Angus <danny@apache.org>
2003-01-06 15:56:24 +0000

    removing avalon dependance in mailets. Step six remove avalon dependance from standard mailets
    git-svn-id: https://svn.apache.org/repos/asf/james/server@108308 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/java/org/apache/james/transport/mailets/AvalonListservManager.java
+++ trunk/src/java/org/apache/james/transport/mailets/AvalonListservManager.java
@@ -7,12 +7,14 @@
  */
 package org.apache.james.transport.mailets;
 
-import org.apache.avalon.framework.component.ComponentException;
-import org.apache.avalon.framework.component.ComponentManager;
+//import org.apache.avalon.framework.component.ComponentException;
+//import org.apache.avalon.framework.component.ComponentManager;
+import javax.mail.MessagingException;
+
 import org.apache.james.Constants;
-import org.apache.james.services.UsersRepository;
-import org.apache.james.services.UsersStore;
+//import org.apache.james.services.UsersStore;
 import org.apache.mailet.MailAddress;
+import org.apache.mailet.UsersRepository;
 
 /**
  * Adds or removes an email address to a listserv.
@@ -23,7 +25,7 @@ import org.apache.mailet.MailAddress;
  * <br>&lt;/mailet&gt;
  *
  * @author  <a href="sergek@lokitech.com">Serge Knystautas </a>
- * @version This is $Revision: 1.6 $
+ * @version This is $Revision: 1.7 $
  */
 public class AvalonListservManager extends GenericListservManager {
 
@@ -33,17 +35,12 @@ public class AvalonListservManager extends GenericListservManager {
      * Initialize the mailet
      */
     public void init() {
-        ComponentManager compMgr = (ComponentManager)getMailetContext().getAttribute(Constants.AVALON_COMPONENT_MANAGER);
-        try {
-            UsersStore usersStore = (UsersStore) compMgr.lookup("org.apache.james.services.UsersStore");
-            String repName = getInitParameter("repositoryName");
+             try {
+                members = getMailetContext().getUserRepository(getInitParameter("repositoryName"));
+            } catch (MessagingException e) {
+                log("init failed cannot access users repository "+getInitParameter("repositoryName"), e);
+            }
 
-            members = (UsersRepository) usersStore.getRepository(repName);
-        } catch (ComponentException cnfe) {
-            log("Failed to retrieve Store component:" + cnfe.getMessage());
-        } catch (Exception e) {
-            log("Failed to retrieve Store component:" + e.getMessage());
-        }
     }
 
     /**
