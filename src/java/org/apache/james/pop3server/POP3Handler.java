9d1f178a60d37c5ae8181a4b8547c262e4ff6737
Danny Angus <danny@apache.org>
2003-01-07 22:42:50 +0000

    Missed this one..removed reference to MailImpl
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@108320 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/james/pop3server/POP3Handler.java
+++ src/java/org/apache/james/pop3server/POP3Handler.java
@@ -383,7 +383,7 @@ public class POP3Handler
         userMailbox.addElement(DELETED);
         for (Iterator it = userInbox.list(); it.hasNext(); ) {
             String key = (String) it.next();
-            MailImpl mc = userInbox.retrieve(key);
+            Mail mc = userInbox.retrieve(key);
             // Retrieve can return null if the mail is no longer in the store.
             // In this case we simply continue to the next key
             if (mc == null) {
