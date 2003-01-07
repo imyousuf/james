2682046698283ef8f073caeae011e1b6206d4b4b
Danny Angus <danny@apache.org>
2003-01-07 11:26:31 +0000

    MailetAPI refactorings Added methods to allow MailImpl to be replaced with Mail in repository interfaces and standard Mailets
    git-svn-id: https://svn.apache.org/repos/asf/james/server@108313 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/java/org/apache/mailet/Mail.java
+++ trunk/src/java/org/apache/mailet/Mail.java
@@ -6,12 +6,11 @@
  * the LICENSE file.
  */
 package org.apache.mailet;
-
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.Serializable;
 import java.util.Collection;
-
+import java.util.Date;
 /**
  * Wrap a MimeMessage with routing information (from SMTP) such
  * as SMTP specified recipients, sender, and ip address and hostname
@@ -28,7 +27,6 @@ public interface Mail extends Serializable, Cloneable {
     String DEFAULT = "root";
     String ERROR = "error";
     String TRANSPORT = "transport";
-
     /**
      * Returns the MimeMessage stored in this message
      *
@@ -36,67 +34,84 @@ public interface Mail extends Serializable, Cloneable {
      * @throws MessagingException - an error occured while loading this object
      */
     MimeMessage getMessage() throws MessagingException;
-
     /**
      * Returns a Collection of MailAddress objects that are recipients of this message
      *
      * @return a Collection of MailAddress objects that are recipients of this message
      */
     Collection getRecipients();
-
+    /**
+     * Method setRecipients.
+     * @param recipients a Collection of MailAddress Objects representing the recipients of this message
+     */
+    void setRecipients(Collection recipients);
     /**
      * The sender of the message, as specified by the MAIL FROM header, or internally defined
      *
      * @return a MailAddress of the sender of this message
      */
     MailAddress getSender();
-
     /**
      * The current state of the message, such as GHOST, ERROR, or DEFAULT
      *
      * @return the state of this message
      */
     String getState();
-
     /**
      * The remote hostname of the server that connected to send this message
      *
      * @return a String of the hostname of the server that connected to send this message
      */
     String getRemoteHost();
-
     /**
      * The remote ip address of the server that connected to send this message
      *
      * @return a String of the ip address of the server that connected to send this message
      */
     String getRemoteAddr();
-
     /**
      * The error message, if any, associated with this message.  Not sure why this is needed.
      *
      * @return a String of a descriptive error message
      */
     String getErrorMessage();
-
     /**
      * Sets the error message associated with this message.  Not sure why this is needed.
      *
      * @param msg - a descriptive error message
      */
     void setErrorMessage(String msg);
-
     /**
      * Sets the MimeMessage associated with this message via the object.
      *
      * @param message - the new MimeMessage that this Mail object will wrap
      */
     void setMessage(MimeMessage message);
-
     /**
      * Sets the state of this message.
      *
      * @param state - the new state of this message
      */
     void setState(String state);
+    /**
+     * Method getUID.
+     * Mainly used to log the progress of mails through processing the string returned need only be unique to this installation of the system.
+     * @return a String Unique Identifier in this system.
+     */
+    String getName();
+    /**
+     * Method setName. Set the internal unique identifier of this Mail which need only be unique to this installation of the system.
+     * @param name the UID
+     */
+    void setName(String name);
+    /**
+     * Method setLastUpdated. Used to set the internal timestamp of the Mail during processing.
+     * @param date 
+     */
+    void setLastUpdated(Date date);
+    /**
+     * Method getLastUpdated.
+     * @return Date. timestamp of the last action which changed this Mail
+     */
+    Date getLastUpdated();
 }
