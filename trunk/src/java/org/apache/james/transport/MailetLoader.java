b4b91c84cb967644d8cdcc53e8996d7f76a262e6
Danny Angus <danny@apache.org>
2003-01-08 12:31:05 +0000

    Mailet Classloader changes. First efforts.. Mailet and matcher classes and their dependancies can now be loaded from ~/apps/james/SAR-INF/classes and jars in ~/apps/james/SAR-INF/lib/
    git-svn-id: https://svn.apache.org/repos/asf/james/server@108328 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/java/org/apache/james/transport/MailetLoader.java
+++ trunk/src/java/org/apache/james/transport/MailetLoader.java
@@ -6,41 +6,48 @@
  * the LICENSE file.
  */
 package org.apache.james.transport;
-
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Vector;
+import javax.mail.MessagingException;
 import org.apache.avalon.framework.component.Component;
 import org.apache.avalon.framework.configuration.Configurable;
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.avalon.framework.context.Context;
+import org.apache.avalon.framework.context.ContextException;
+import org.apache.avalon.framework.context.Contextualizable;
+import org.apache.avalon.framework.logger.Logger;
+import org.apache.avalon.phoenix.BlockContext;
 import org.apache.james.core.MailetConfigImpl;
 import org.apache.mailet.Mailet;
 import org.apache.mailet.MailetContext;
 import org.apache.mailet.MailetException;
-
-import javax.mail.MessagingException;
-import java.util.Vector;
-
 /**
  * Loads Mailets for use inside James.
  *
  * @author Serge Knystautas <sergek@lokitech.com>
  * @author Federico Barbieri <scoobie@systemy.it>
  */
-public class MailetLoader implements Component, Configurable {
-
+public class MailetLoader implements Component, Configurable, Contextualizable {
+    private ClassLoader theClassLoader = null;
+    private String baseDirectory = null;
+    private Logger logger;
     /**
      * The list of packages that may contain Mailets
      */
     private Vector mailetPackages;
-
     /**
      * @see org.apache.avalon.framework.configuration.Configurable#configure(Configuration)
      */
     public void configure(Configuration conf) throws ConfigurationException {
         mailetPackages = new Vector();
         mailetPackages.addElement("");
-        final Configuration[] pkgConfs = conf.getChildren( "mailetpackage" );
-        for ( int i = 0; i < pkgConfs.length; i++ )
-        {
+        final Configuration[] pkgConfs = conf.getChildren("mailetpackage");
+        for (int i = 0; i < pkgConfs.length; i++) {
             Configuration c = pkgConfs[i];
             String packageName = c.getValue();
             if (!packageName.endsWith(".")) {
@@ -48,8 +55,34 @@ public class MailetLoader implements Component, Configurable {
             }
             mailetPackages.addElement(packageName);
         }
+        File base = new File(baseDirectory + "/SAR-INF/lib");
+        String[] flist = base.list();
+        Vector jarlist = new Vector();
+        URL[] classPath = null;
+        try {
+            jarlist.add(new URL("file://" + baseDirectory + "/SAR-INF/classes/"));
+        } catch (MalformedURLException e) {
+            logger.error(
+                "cant add "
+                    + "file://"
+                    + baseDirectory
+                    + "/SAR-INF/classes/ to mailet classloader");
+        }
+        if (flist != null) {
+            for (int i = 0; i < flist.length; i++) {
+                try {
+                    if (flist[i].indexOf("jar") == flist[i].length() - 3) {
+                        jarlist.add(new URL("file://" + flist[i]));
+                        logger.debug("added " + flist[i] + " to Mailet Classloader");
+                    }
+                } catch (MalformedURLException e) {
+                    logger.error("cant add " + "file://" + flist[i] + " to mailet classloader");
+                }
+            }
+        }
+        classPath = (URL[]) jarlist.toArray(new URL[jarlist.size()]);
+        theClassLoader = new URLClassLoader(classPath, this.getClass().getClassLoader());
     }
-
     /**
      * Get a new Mailet with the specified name acting
      * in the specified context.
@@ -63,18 +96,15 @@ public class MailetLoader implements Component, Configurable {
         throws MessagingException {
         try {
             for (int i = 0; i < mailetPackages.size(); i++) {
-                String className = (String)mailetPackages.elementAt(i) + mailetName;
-                ClassLoader theClassLoader = null;
+                String className = (String) mailetPackages.elementAt(i) + mailetName;
                 try {
                     MailetConfigImpl configImpl = new MailetConfigImpl();
                     configImpl.setMailetName(mailetName);
                     configImpl.setConfiguration(configuration);
                     configImpl.setMailetContext(context);
-
                     if (theClassLoader == null) {
                         theClassLoader = this.getClass().getClassLoader();
                     }
-
                     Mailet mailet = (Mailet) theClassLoader.loadClass(className).newInstance();
                     mailet.init(configImpl);
                     return mailet;
@@ -84,20 +114,32 @@ public class MailetLoader implements Component, Configurable {
             }
             StringBuffer exceptionBuffer =
                 new StringBuffer(128)
-                        .append("Requested mailet not found: ")
-                        .append(mailetName)
-                        .append(".  looked in ")
-                        .append(mailetPackages.toString());
+                    .append("Requested mailet not found: ")
+                    .append(mailetName)
+                    .append(".  looked in ")
+                    .append(mailetPackages.toString());
             throw new ClassNotFoundException(exceptionBuffer.toString());
         } catch (MessagingException me) {
             throw me;
         } catch (Exception e) {
             StringBuffer exceptionBuffer =
-                new StringBuffer(128)
-                        .append("Could not load mailet (")
-                        .append(mailetName)
-                        .append(")");
+                new StringBuffer(128).append("Could not load mailet (").append(mailetName).append(
+                    ")");
             throw new MailetException(exceptionBuffer.toString(), e);
         }
+    }
+    /**
+         * @see org.apache.avalon.framework.context.Contextualizable#contextualize(Context)
+         */
+    public void contextualize(final Context context) throws ContextException {
+        try {
+            baseDirectory = ((BlockContext) context).getBaseDirectory().getCanonicalPath();
+        } catch (IOException e) {
+            logger.error("cant get base directory for mailet loader");
+            throw new ContextException("cant contextualise loader " + e.getMessage(), e);
+        }
+    }
+    public void setLogger(Logger logger) {
+        this.logger = logger;
     }
 }
--- trunk/src/java/org/apache/james/transport/MatchLoader.java
+++ trunk/src/java/org/apache/james/transport/MatchLoader.java
@@ -6,41 +6,49 @@
  * the LICENSE file.
  */
 package org.apache.james.transport;
-
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Vector;
+import javax.mail.MessagingException;
 import org.apache.avalon.framework.component.Component;
 import org.apache.avalon.framework.configuration.Configurable;
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.avalon.framework.context.Context;
+import org.apache.avalon.framework.context.ContextException;
+import org.apache.avalon.framework.context.Contextualizable;
+import org.apache.avalon.framework.logger.AbstractLogEnabled;
+import org.apache.avalon.framework.logger.Logger;
+import org.apache.avalon.phoenix.BlockContext;
 import org.apache.james.core.MatcherConfigImpl;
 import org.apache.mailet.MailetContext;
 import org.apache.mailet.MailetException;
 import org.apache.mailet.Matcher;
-
-import javax.mail.MessagingException;
-import java.util.Vector;
-
 /**
  * Loads Matchers for use inside James.
  *
  * @author Serge Knystautas <sergek@lokitech.com>
  * @author Federico Barbieri <scoobie@systemy.it>
  */
-public class MatchLoader implements Component, Configurable {
-
+public class MatchLoader implements Component, Configurable, Contextualizable {
     /**
      * The list of packages that may contain Mailets
      */
     private Vector matcherPackages;
-
+    private ClassLoader theClassLoader = null;
+    private String baseDirectory = null;
+    private Logger logger;
     /**
      * @see org.apache.avalon.framework.configuration.Configurable#configure(Configuration)
      */
     public void configure(Configuration conf) throws ConfigurationException {
         matcherPackages = new Vector();
         matcherPackages.addElement("");
-        final Configuration[] pkgConfs = conf.getChildren( "matcherpackage" );
-        for ( int i = 0; i < pkgConfs.length; i++ )
-        {
+        final Configuration[] pkgConfs = conf.getChildren("matcherpackage");
+        for (int i = 0; i < pkgConfs.length; i++) {
             Configuration c = pkgConfs[i];
             String packageName = c.getValue();
             if (!packageName.endsWith(".")) {
@@ -48,8 +56,34 @@ public class MatchLoader implements Component, Configurable {
             }
             matcherPackages.addElement(packageName);
         }
+        File base = new File(baseDirectory + "/SAR-INF/lib");
+        String[] flist = base.list();
+        Vector jarlist = new Vector();
+        URL[] classPath = null;
+        try {
+            jarlist.add(new URL("file://" + baseDirectory + "/SAR-INF/lib/classes/"));
+        } catch (MalformedURLException e) {
+            logger.error(
+                "cant add "
+                    + "file://"
+                    + baseDirectory
+                    + "/SAR-INF/classes/ to matcher classloader");
+        }
+        if (flist != null) {
+            for (int i = 0; i < flist.length; i++) {
+                try {
+                    if (flist[i].indexOf("jar") == flist[i].length() - 3) {
+                        jarlist.add(new URL("file://" + flist[i]));
+                        logger.debug("added " + flist[i] + " to Matcher Classloader");
+                    }
+                } catch (MalformedURLException e) {
+                    logger.error("cant add " + "file://" + flist[i] + " to matcher classloader");
+                }
+            }
+        }
+        classPath = (URL[]) jarlist.toArray(new URL[jarlist.size()]);
+        theClassLoader = new URLClassLoader(classPath, this.getClass().getClassLoader());
     }
-
     /**
      * Get a new Matcher with the specified name acting
      * in the specified context.
@@ -59,8 +93,7 @@ public class MatchLoader implements Component, Configurable {
      *                matcher
      * @throws MessagingException if an error occurs
      */
-    public Matcher getMatcher(String matchName, MailetContext context)
-        throws MessagingException {
+    public Matcher getMatcher(String matchName, MailetContext context) throws MessagingException {
         try {
             String condition = (String) null;
             int i = matchName.indexOf('=');
@@ -68,19 +101,13 @@ public class MatchLoader implements Component, Configurable {
                 condition = matchName.substring(i + 1);
                 matchName = matchName.substring(0, i);
             }
-            ClassLoader theClassLoader = null;
             for (i = 0; i < matcherPackages.size(); i++) {
-                String className = (String)matcherPackages.elementAt(i) + matchName;
+                String className = (String) matcherPackages.elementAt(i) + matchName;
                 try {
                     MatcherConfigImpl configImpl = new MatcherConfigImpl();
                     configImpl.setMatcherName(matchName);
                     configImpl.setCondition(condition);
                     configImpl.setMailetContext(context);
-
-                    if (theClassLoader == null) {
-                        theClassLoader = this.getClass().getClassLoader();
-                    }
-
                     Matcher matcher = (Matcher) theClassLoader.loadClass(className).newInstance();
                     matcher.init(configImpl);
                     return matcher;
@@ -90,20 +117,32 @@ public class MatchLoader implements Component, Configurable {
             }
             StringBuffer exceptionBuffer =
                 new StringBuffer(128)
-                        .append("Requested matcher not found: ")
-                        .append(matchName)
-                        .append(".  looked in ")
-                        .append(matcherPackages.toString());
+                    .append("Requested matcher not found: ")
+                    .append(matchName)
+                    .append(".  looked in ")
+                    .append(matcherPackages.toString());
             throw new ClassNotFoundException(exceptionBuffer.toString());
         } catch (MessagingException me) {
             throw me;
         } catch (Exception e) {
             StringBuffer exceptionBuffer =
-                new StringBuffer(128)
-                        .append("Could not load matcher (")
-                        .append(matchName)
-                        .append(")");
+                new StringBuffer(128).append("Could not load matcher (").append(matchName).append(
+                    ")");
             throw new MailetException(exceptionBuffer.toString(), e);
         }
+    }
+    /**
+     * @see org.apache.avalon.framework.context.Contextualizable#contextualize(Context)
+     */
+    public void contextualize(final Context context) throws ContextException {
+        try {
+            baseDirectory = ((BlockContext) context).getBaseDirectory().getCanonicalPath();
+        } catch (IOException e) {
+            logger.error("cant get base directory for matcher loader");
+            throw new ContextException("cant contextualise loader " + e.getMessage(), e);
+        }
+    }
+    public void setLogger(Logger logger) {
+        this.logger = logger;
     }
 }
