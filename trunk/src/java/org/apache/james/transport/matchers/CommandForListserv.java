40ac1632b7bb3fbf91a7d2c876f1131f9895128a
Noel J. Bergman <noel@apache.org>
2003-02-06 06:43:01 +0000

    generated docs
    git-svn-id: https://svn.apache.org/repos/asf/james/server@108439 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/java/org/apache/james/transport/matchers/CommandForListserv.java
+++ trunk/src/java/org/apache/james/transport/matchers/CommandForListserv.java
@@ -27,9 +27,9 @@ public class CommandForListserv extends GenericRecipientMatcher {
     }
 
     public boolean matchRecipient(MailAddress recipient) {
-        if (recipient.getHost().equals(listservAddress.getHost())) {
-            if (recipient.getUser().equals(listservAddress.getUser() + "-on")
-                || recipient.getUser().equals(listservAddress.getUser() + "-off")) {
+        if (recipient.getHost().equalsIgnoreCase(listservAddress.getHost())) {
+            if (recipient.getUser().equalsIgnoreCase(listservAddress.getUser() + "-on")
+                || recipient.getUser().equalsIgnoreCase(listservAddress.getUser() + "-off")) {
                 return true;
             }
         }
