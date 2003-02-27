7ad41a1b732ee593728c02ac9e13db26237d78b1
Noel J. Bergman <noel@apache.org>
2003-02-27 06:23:50 +0000

    Added the full ASF License to all Java files
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@108541 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/james/transport/matchers/RemoteAddrNotInNetwork.java
+++ src/java/org/apache/james/transport/matchers/RemoteAddrNotInNetwork.java
@@ -1,77 +1,89 @@
-/*
- * Copyright (C) The Apache Software Foundation. All rights reserved.
+/* ====================================================================
+ * The Apache Software License, Version 1.1
  *
- * This software is published under the terms of the Apache Software License
- * version 1.1, a copy of which has been included with this distribution in
- * the LICENSE file.
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache", "Jakarta", "JAMES" and "Apache Software Foundation"
+ *    must not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * Portions of this software are based upon public domain software
+ * originally written at the National Center for Supercomputing Applications,
+ * University of Illinois, Urbana-Champaign.
  */
+
 package org.apache.james.transport.matchers;
 
-import org.apache.mailet.GenericMatcher;
 import org.apache.mailet.Mail;
-
 import javax.mail.MessagingException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.StringTokenizer;
-import java.util.Vector;
 
 /**
- * Checks the IP address of the sending server against a comma-
- * delimited list of IP addresses or domain names.
- * <P>Networks should be indicated with a wildcard *, e.g. 192.168.*
- * <br>Note: The wildcard can go at any level, the matcher will match if the
- * sending host's IP address (as a String based on the octet representation)
- * starts with the String indicated in the configuration file, excluding the
- * wildcard.
- * <p>Multiple addresses can be indicated, e.g: '127.0.0.1,192.168.*,domain.tld'
- *
- */
-public class RemoteAddrNotInNetwork extends GenericMatcher {
-    private Collection networks = null;
+  * <P>This implments the CIDR ( slash notation ) method for determining sub-networks.
+  * <UL>Networks can be indicated with
+  *   <LI>wildcards(*) 192.168.*
+  *   <LI>by name 'domain.tld'
+  *   <LI>slash notation 192.168.100.0/24
+  *   </UL>
+  * <BR>Note: All representations will be converted to CIDR notation and matches are made
+  *   based on a target IPAddress against a CIDR string.
+  * <BR>Multiple addresses can be indicated, e.g: '127.0.0.1,192.168.*,domain.tld'
+  *
+  * Checks the IP address of the sending server against the CIDR
+  * Collection, returns null if there is a match.
+  * 
+  * @see GenericNetworkMatcher.init()
+  */
 
-    public void init() throws MessagingException {
-        StringTokenizer st = new StringTokenizer(getCondition(), ", ", false);
-        networks = new Vector();
-        while (st.hasMoreTokens()) {
-            String addr = st.nextToken();
-            if (addr.equals("127.0.0.1")) {
-                //Add address of local machine as a match
-                try {
-                    InetAddress localaddr = InetAddress.getLocalHost();
-                    networks.add(localaddr.getHostAddress());
-                } catch (UnknownHostException uhe) {
-                }
-            }
-
-            try {
-                if (addr.endsWith("*")) {
-                    addr = addr.substring(0, addr.length() - 1);
-                }
-                else {
-                    addr = InetAddress.getByName(addr).getHostAddress();
-                }
-                networks.add(addr);
-            } catch (UnknownHostException uhe) {
-                log("Cannot match against invalid domain: " + uhe.getMessage());
-            }
-        }
-    }
-
-    public Collection match(Mail mail) {
-        String host = mail.getRemoteAddr();
-        //Check to see whether it's in any of the networks... needs to be smarter to
-        // support subnets better
-        for (Iterator i = networks.iterator(); i.hasNext(); ) {
-            String invalidNetwork = i.next().toString();
-            if (host.startsWith(invalidNetwork)) {
-                //This is in this network... that's all we need for a failed match
-                return null;
-            }
-        }
-        //Could not match this to any network
-        return mail.getRecipients();
+public class RemoteAddrNotInNetwork extends GenericNetworkMatcher {
+    public Collection match(Mail mail) throws MessagingException {
+        // if it matches, then we do NOT want to execute the mailet
+        return matchNetwork(mail) ? null : mail.getRecipients();
     }
 }
+
+
