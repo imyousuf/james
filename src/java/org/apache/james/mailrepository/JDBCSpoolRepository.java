4de3b06e460b29835b57aef8be04ae6a2f7d426e
Noel J. Bergman <noel@apache.org>
2003-05-07 06:17:10 +0000

    Fixed newly introduced error adjusting the timeout for accept(long delay)
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@108604 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/james/mailrepository/JDBCSpoolRepository.java
+++ src/java/org/apache/james/mailrepository/JDBCSpoolRepository.java
@@ -215,8 +215,7 @@ public class JDBCSpoolRepository extends JDBCMailRepository implements SpoolRepo
 //            }
             try {
                 synchronized (this) {
-                    long waitTime = sleepUntil - System.currentTimeMillis();
-                    //StringBuffer errorBuffer =
+                    long waitTime = (sleepUntil == 0) ? 0 : sleepUntil - System.currentTimeMillis();                    //StringBuffer errorBuffer =
                     //    new StringBuffer(128)
                     //            .append("waiting ")
                     //            .append((waitTime) / 1000L)
