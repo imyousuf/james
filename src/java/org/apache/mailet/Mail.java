dd286eb1af51db838b8686632b81d3cbf07458d3
Vincenzo Gianferrari Pini <vincenzo@apache.org>
2003-07-21 10:51:26 +0000

    Adding "@since Mailet API v2.1" to the javadoc of all the new mail attributes related methods.
    git-svn-id: https://svn.apache.org/repos/asf/james/server/branches/branch_2_1_fcs@108904 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/mailet/Mail.java
+++ src/java/org/apache/mailet/Mail.java
@@ -71,7 +71,7 @@ import java.util.Iterator;
  * which processor in the mailet container it is currently running.
  * Special processor names are "root" and "error".
  *
- * @version CVS $Revision: 1.3.4.3 $ $Date: 2003/07/15 10:12:45 $
+ * @version CVS $Revision: 1.3.4.4 $ $Date: 2003/07/21 10:51:26 $
  */
 public interface Mail extends Serializable, Cloneable {
     String GHOST = "ghost";
@@ -164,6 +164,7 @@ public interface Mail extends Serializable, Cloneable {
      * @param name - a String specifying the name of the attribute
      * @return an Object containing the value of the attribute, or null if no attribute
      *      exists matching the given name
+     * @since Mailet API v2.1
      */
     Serializable getAttribute(String name);
 
@@ -173,11 +174,13 @@ public interface Mail extends Serializable, Cloneable {
      * attribute name to get the value of an attribute.
      *
      * @return an Iterator of attribute names
+     * @since Mailet API v2.1
      */
     Iterator getAttributeNames();
 
     /**
      * @return true if this Mail instance has any attributes set.
+     * @since Mailet API v2.1
      **/
     boolean hasAttributes();
     
@@ -190,11 +193,13 @@ public interface Mail extends Serializable, Cloneable {
      * @return previous attribute value associated with specified name, or null
      * if there was no mapping for name (null can also mean that null
      * was bound to the name)
+     * @since Mailet API v2.1
      */
     Serializable removeAttribute(String name);
 
     /**
      * Removes all the attributes associated with this Mail instance.  
+     * @since Mailet API v2.1
      **/
     void removeAllAttributes();
     
@@ -205,13 +210,15 @@ public interface Mail extends Serializable, Cloneable {
      * As instances of Mail is Serializable, it is necessary that the attributes being
      * Serializable as well
      * <p>
-     * Attribute names should follow the same convention as package names. The Java
-     * Mailet API specification reserves names matching java.*, javax.*, and sun.*.
+     * Attribute names should follow the same convention as package names.
+     * The Mailet API specification reserves names matching <I>org.apache.james.*</I>
+     * and <I>org.apache.mailet.*</I>.
      *
      * @param name - a String specifying the name of the attribute
      * @param object - a Serializable Object representing the attribute to be bound
      * @return the object previously bound to the name, null if the name was
      * not bound (null can also mean that null was bound to the name)
+     * @since Mailet API v2.1
      */
     Serializable setAttribute(String name, Serializable object);
 }
