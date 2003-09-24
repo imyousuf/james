43c72b815ab1eb34e868540afadf00e477f2c600
Noel J. Bergman <noel@apache.org>
2003-09-24 21:11:26 +0000

    Allow specifying networks to be considered bad.  Undocumented, since this may not be the *best* way, but available
    git-svn-id: https://svn.apache.org/repos/asf/james/server/branches/branch_2_1_fcs@108976 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/james/transport/matchers/SenderInFakeDomain.java
+++ src/java/org/apache/james/transport/matchers/SenderInFakeDomain.java
@@ -68,9 +68,8 @@ import java.util.Collection;
  * there are no entries, the domain is considered fake and the match is 
  * successful.
  *
- * @author  Serge Knystautas <sergek@lokitech.com>
  */
-public class SenderInFakeDomain extends GenericMatcher {
+public class SenderInFakeDomain extends AbstractNetworkMatcher {
 
     public Collection match(Mail mail) {
         if (mail.getSender() == null) {
@@ -83,8 +82,29 @@ public class SenderInFakeDomain extends GenericMatcher {
             //No records...could not deliver to this domain, so matches criteria.
             log("No MX, A, or CNAME record found for domain: " + domain);
             return mail.getRecipients();
+        } else if (matchNetwork(servers.iterator().next().toString())){
+            /*
+             * It could be a wildcard address like these:
+             *
+             * 64.55.105.9/32          # Allegiance Telecom Companies Worldwide (.nu)
+             * 64.94.110.11/32         # VeriSign (.com .net)
+             * 194.205.62.122/32       # Network Information Center - Ascension Island (.ac)
+             * 194.205.62.62/32        # Internet Computer Bureau (.sh)
+             * 195.7.77.20/32          # Fredrik Reutersward Data (.museum)
+             * 206.253.214.102/32      # Internap Network Services (.cc)
+             * 212.181.91.6/32         # .NU Domain Ltd. (.nu)
+             * 219.88.106.80/32        # Telecom Online Solutions (.cx)
+             * 194.205.62.42/32        # Internet Computer Bureau (.tm)
+             * 216.35.187.246/32       # Cable & Wireless (.ws)
+             * 203.119.4.6/32          # .PH TLD (.ph)
+             *
+             */
+            log("Banned IP found for domain: " + domain);
+            log(" --> :" + servers.iterator().next().toString());
+            return mail.getRecipients();
         } else {
-            //Some servers were found... the domain is not fake.
+            // Some servers were found... the domain is not fake.
+
             return null;
         }
     }
