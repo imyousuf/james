c9dadda28858431a311e6db7b5cb19e94c6c7cfe
Noel J. Bergman <noel@apache.org>
2003-09-24 16:11:39 +0000

    Initial version of a mailet that can respool from a repository.
    git-svn-id: https://svn.apache.org/repos/asf/james/server@108974 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/branch_2_1_fcs/src/java/org/apache/james/transport/mailets/FromRepository.java
@@ -0,0 +1,175 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2000-2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache", "Jakarta", "JAMES" and "Apache Software Foundation"
+ *    must not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * Portions of this software are based upon public domain software
+ * originally written at the National Center for Supercomputing Applications,
+ * University of Illinois, Urbana-Champaign.
+ */
+
+package org.apache.james.transport.mailets;
+
+import org.apache.avalon.framework.component.ComponentException;
+import org.apache.avalon.framework.component.ComponentManager;
+import org.apache.avalon.framework.configuration.DefaultConfiguration;
+import org.apache.james.Constants;
+import org.apache.james.core.MailImpl;
+import org.apache.james.services.MailRepository;
+import org.apache.james.services.MailStore;
+import org.apache.james.util.RFC2822Headers;
+import org.apache.mailet.GenericMailet;
+import org.apache.mailet.MailAddress;
+import org.apache.mailet.Mail;
+import javax.mail.internet.InternetAddress;
+import javax.mail.MessagingException;
+import java.util.Iterator;
+
+/**
+ * Re-spools Mail found in the specified Repository.
+ *
+ * &lt;mailet match="RecipientIs=respool@localhost" class="FromRepository"&gt;
+ *    &lt;repositoryPath&gt; <i>repository path</i> &lt;/repositoryPath&gt;
+ *    &lt;delete [true|<b>false</b>] &lt;/delete&gt;
+ * &lt;/mailet&gt;
+ *
+ * @version This is $Revision: 1.1.2.1 $
+ */
+public class FromRepository extends GenericMailet {
+
+    /**
+     * The repository from where this mailet spools mail.
+     */
+    private MailRepository repository;
+
+    /**
+     * Whether this mailet should delete messages after being spooled
+     */
+    private boolean delete = true;
+
+    /**
+     * The path to the repository
+     */
+    private String repositoryPath;
+
+    /**
+     * Initialize the mailet, loading configuration information.
+     */
+    public void init() {
+        repositoryPath = getInitParameter("repositoryPath");
+        try {
+            delete = new Boolean(getInitParameter("delete")).booleanValue();
+        } catch (Exception e) {
+            // Ignore exception, default to true
+        }
+
+        ComponentManager compMgr = (ComponentManager)getMailetContext().getAttribute(Constants.AVALON_COMPONENT_MANAGER);
+        try {
+            MailStore mailstore = (MailStore) compMgr.lookup("org.apache.james.services.MailStore");
+            DefaultConfiguration mailConf
+                = new DefaultConfiguration("repository", "generated:ToRepository");
+            mailConf.setAttribute("destinationURL", repositoryPath);
+            mailConf.setAttribute("type", "MAIL");
+            repository = (MailRepository) mailstore.select(mailConf);
+        } catch (ComponentException cnfe) {
+            log("Failed to retrieve Store component:" + cnfe.getMessage());
+        } catch (Exception e) {
+            log("Failed to retrieve Store component:" + e.getMessage());
+        }
+
+    }
+
+    /**
+     * Spool mail from a particular repository.
+     *
+     * @param triggering e-mail (eventually parameterize via the
+     * trigger message)
+     */
+    public void service(Mail trigger) throws MessagingException {
+        trigger.setState(Mail.GHOST);
+        java.util.Collection processed = new java.util.ArrayList();
+        Iterator list = repository.list();
+        while (list.hasNext()) {
+            String key = (String) list.next();
+            try {
+                MailImpl mail =  repository.retrieve(key);
+                if (mail != null && mail.getRecipients() != null) {
+                    log((new StringBuffer(160).append("Spooling mail ").append(mail.getName()).append(" from ").append(repositoryPath)).toString());
+
+                    /*
+                    log("Return-Path: " + mail.getMessage().getHeader(RFC2822Headers.RETURN_PATH, ", "));
+                    log("Sender: " + mail.getSender());
+                    log("To: " + mail.getMessage().getHeader(RFC2822Headers.TO, ", "));
+                    log("Recipients: ");
+                    for (Iterator i = mail.getRecipients().iterator(); i.hasNext(); ) {
+                        log("    " + ((MailAddress)i.next()).toString());
+                    };
+                    */
+
+                    getMailetContext().sendMail(mail);
+                    if (delete) processed.add(key);
+                }
+            } catch (MessagingException e) {
+                log((new StringBuffer(160).append("Unable to re-spool mail ").append(key).append(" from ").append(repositoryPath)).toString(), e);
+            }
+        }
+        if (delete) repository.remove(processed);
+    }
+
+    /**
+     * Return a string describing this mailet.
+     *
+     * @return a string describing this mailet
+     */
+    public String getMailetInfo() {
+        return "FromRepository Mailet";
+    }
+}
