4871563c264358942f4e1cd7ca319709ec41ffd1
Noel J. Bergman <noel@apache.org>
2005-05-03 05:58:13 +0000

    merge pop3server from v2 and trunk.  avalon updates
    git-svn-id: https://svn.apache.org/repos/asf/james/server@167866 13f79535-47bb-0310-9956-ffa450edef68

--- branches/merge_v2_and_trunk/src/java/org/apache/james/pop3server/POP3Handler.java
+++ branches/merge_v2_and_trunk/src/java/org/apache/james/pop3server/POP3Handler.java
@@ -1,5 +1,5 @@
 /***********************************************************************
- * Copyright (c) 2000-2004 The Apache Software Foundation.             *
+ * Copyright (c) 1999-2005 The Apache Software Foundation.             *
  * All rights reserved.                                                *
  * ------------------------------------------------------------------- *
  * Licensed under the Apache License, Version 2.0 (the "License"); you *
