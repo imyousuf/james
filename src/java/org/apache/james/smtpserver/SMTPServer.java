cd53305f2e2b90d6312c61c130256e3aae7ec10d
Vincenzo Gianferrari Pini <vincenzo@apache.org>
2005-05-03 15:15:56 +0000

    Some fixes to get compiled
    git-svn-id: https://svn.apache.org/repos/asf/james/server/branches/merge_v2_and_trunk@167921 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/james/smtpserver/SMTPServer.java
+++ src/java/org/apache/james/smtpserver/SMTPServer.java
@@ -50,7 +50,7 @@ import org.apache.mailet.MailetContext;
  * IMPORTANT: SMTPServer extends AbstractJamesService.  If you implement ANY
  * lifecycle methods, you MUST call super.<method> as well.
  */
-public class SMTPServer extends AbstractJamesService implements Component, SMTPServerMBean {
+public class SMTPServer extends AbstractJamesService implements SMTPServerMBean {
 
     /**
      * The mailet context - we access it here to set the hello name for the Mailet API
