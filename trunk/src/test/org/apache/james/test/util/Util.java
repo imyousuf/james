baba7d6e6f9ee4444ad49f6ede46fbf4fb755dca
Stefano Bagnara <bago@apache.org>
2006-01-02 18:25:59 +0000

    Introduce Unit Testing (JAMES-427) Thanks to Bernd Fondermann. Running tests require junit and ristretto (not committed by now)
    git-svn-id: https://svn.apache.org/repos/asf/james/server@365388 13f79535-47bb-0310-9956-ffa450edef68

---
+++ trunk/src/test/org/apache/james/test/util/Util.java
@@ -0,0 +1,33 @@
+/***********************************************************************
+ * Copyright (c) 1999-2005 The Apache Software Foundation.             *
+ * All rights reserved.                                                *
+ * ------------------------------------------------------------------- *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you *
+ * may not use this file except in compliance with the License. You    *
+ * may obtain a copy of the License at:                                *
+ *                                                                     *
+ *     http://www.apache.org/licenses/LICENSE-2.0                      *
+ *                                                                     *
+ * Unless required by applicable law or agreed to in writing, software *
+ * distributed under the License is distributed on an "AS IS" BASIS,   *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or     *
+ * implied.  See the License for the specific language governing       *
+ * permissions and limitations under the License.                      *
+ ***********************************************************************/
+package org.apache.james.test.util;
+
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.DefaultConfiguration;
+
+public class Util {
+    
+    public static int getRandomNonPrivilegedPort() {
+        return ((int)( Math.random() * 1000) + 3000);
+    }
+    
+    public static Configuration getValuedConfiguration(String name, String value) {
+        DefaultConfiguration defaultConfiguration = new DefaultConfiguration(name);
+        defaultConfiguration.setValue(value);
+        return defaultConfiguration;
+    }
+}
