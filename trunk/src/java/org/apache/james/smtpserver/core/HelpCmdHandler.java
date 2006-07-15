61c8799b430408e002a4dabce5989ca8d8f39d3c
Norman Maurer <norman@apache.org>
2006-07-15 09:16:58 +0000

    Start to merge the smtp-fast-fail-api
    git-svn-id: https://svn.apache.org/repos/asf/james/server@422187 13f79535-47bb-0310-9956-ffa450edef68

---
+++ trunk/src/java/org/apache/james/smtpserver/core/HelpCmdHandler.java
@@ -0,0 +1,56 @@
+/***********************************************************************
+ * Copyright (c) 1999-2006 The Apache Software Foundation.             *
+ * All rights reserved.                                                *
+ * ------------------------------------------------------------------- *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you *
+ * may not use this file except in compliance with the License. You    *
+ * may obtain a copy of the License at:                                *
+ *                                                                     *
+ *     http://www.apache.org/licenses/LICENSE-2.0                      *
+ *                                                                     *
+ * Unless required by applicable law or agreed to in writing, software *
+ * distributed under the License is distributed on an "AS IS" BASIS,   *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or     *
+ * implied.  See the License for the specific language governing       *
+ * permissions and limitations under the License.                      *
+ ***********************************************************************/
+
+package org.apache.james.smtpserver.core;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.james.smtpserver.CommandHandler;
+import org.apache.james.smtpserver.SMTPSession;
+import org.apache.james.util.mail.dsn.DSNStatus;
+
+/**
+  * Handles HELP command
+  */
+public class HelpCmdHandler implements CommandHandler {
+    /**
+     * The name of the command handled by the command handler
+     */
+    private final static String COMMAND_NAME = "HELP";
+
+
+    /*
+     * handles HELP command
+     *
+     * @see org.apache.james.smtpserver.CommandHandler#onCommand(SMTPSession)
+    **/
+    public void onCommand(SMTPSession session) {
+        String responseString = "502 "+DSNStatus.getStatus(DSNStatus.PERMANENT,DSNStatus.SYSTEM_NOT_CAPABLE)+" HELP is not supported";
+        session.writeResponse(responseString);
+    }
+
+    /**
+     * @see org.apache.james.smtpserver.CommandHandler#getImplCommands()
+     */
+    public Collection getImplCommands() {
+        Collection implCommands = new ArrayList();
+        implCommands.add("HELP");
+        
+        return implCommands;
+    }
+}
---
+++ trunk/src/java/org/apache/james/smtpserver/core/QuitCmdHandler.java
@@ -0,0 +1,83 @@
+/***********************************************************************
+ * Copyright (c) 1999-2006 The Apache Software Foundation.             *
+ * All rights reserved.                                                *
+ * ------------------------------------------------------------------- *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you *
+ * may not use this file except in compliance with the License. You    *
+ * may obtain a copy of the License at:                                *
+ *                                                                     *
+ *     http://www.apache.org/licenses/LICENSE-2.0                      *
+ *                                                                     *
+ * Unless required by applicable law or agreed to in writing, software *
+ * distributed under the License is distributed on an "AS IS" BASIS,   *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or     *
+ * implied.  See the License for the specific language governing       *
+ * permissions and limitations under the License.                      *
+ ***********************************************************************/
+
+package org.apache.james.smtpserver.core;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.james.smtpserver.CommandHandler;
+import org.apache.james.smtpserver.SMTPSession;
+import org.apache.james.util.mail.dsn.DSNStatus;
+
+/**
+  * Handles QUIT command
+  */
+public class QuitCmdHandler implements CommandHandler {
+
+    /**
+     * The name of the command handled by the command handler
+     */
+    private final static String COMMAND_NAME = "QUIT";
+
+    /**
+     * handles QUIT command
+     *
+     * @see org.apache.james.smtpserver.CommandHandler#onCommand(SMTPSession)
+     */
+    public void onCommand(SMTPSession session) {
+        doQUIT(session, session.getCommandArgument());
+
+    }
+
+
+    /**
+     * Handler method called upon receipt of a QUIT command.
+     * This method informs the client that the connection is
+     * closing.
+     *
+     * @param session SMTP session object
+     * @param argument the argument passed in with the command by the SMTP client
+     */
+    private void doQUIT(SMTPSession session, String argument) {
+
+        String responseString = "";
+        if ((argument == null) || (argument.length() == 0)) {
+            session.getResponseBuffer().append("221 "+DSNStatus.getStatus(DSNStatus.SUCCESS,DSNStatus.UNDEFINED_STATUS)+" ")
+                          .append(session.getConfigurationData().getHelloName())
+                          .append(" Service closing transmission channel");
+            responseString = session.clearResponseBuffer();
+        } else {
+            responseString = "500 "+DSNStatus.getStatus(DSNStatus.PERMANENT,DSNStatus.DELIVERY_INVALID_ARG)+" Unexpected argument provided with QUIT command";
+        }
+        session.writeResponse(responseString);
+        session.endSession();
+    }
+
+    /**
+     * @see org.apache.james.smtpserver.CommandHandler#getImplCommands()
+     */
+    public Collection getImplCommands() {
+        Collection implCommands = new ArrayList();
+        implCommands.add("QUIT");
+        
+        return implCommands;
+    }
+}
+
+
+
