02b011d02355160ff497fe26e50fea0b42acda3e
Norman Maurer <norman@apache.org>
2006-08-11 06:35:09 +0000

    Add junit test for ValidRcptMX fastfail handler
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@430695 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/james/smtpserver/ValidRcptMXTest.java
@@ -0,0 +1,126 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.smtpserver;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.mail.internet.ParseException;
+
+import org.apache.avalon.framework.container.ContainerUtil;
+import org.apache.james.services.DNSServer;
+import org.apache.james.smtpserver.core.filter.fastfail.ValidRcptMX;
+import org.apache.james.test.mock.avalon.MockLogger;
+import org.apache.mailet.MailAddress;
+
+import junit.framework.TestCase;
+
+public class ValidRcptMXTest extends TestCase {
+    private String response = null;
+
+    private final static String INVALID_HOST = "invalid.host.de";
+
+    private final static String INVALID_MX = "mx." + INVALID_HOST;
+
+    private final static String LOOPBACK = "127.0.0.1";
+
+    protected void setUp() throws Exception {
+	response = null;
+
+	super.setUp();
+    }
+
+    private SMTPSession setupMockedSMTPSession(final MailAddress rcpt) {
+	SMTPSession session = new AbstractSMTPSession() {
+	    HashMap state = new HashMap();
+
+	    public Map getState() {
+		state.put(SMTPSession.CURRENT_RECIPIENT, rcpt);
+		return state;
+	    }
+
+	    public void writeResponse(String resp) {
+		response = resp;
+	    }
+
+	};
+	return session;
+    }
+
+    private DNSServer setupMockedDNSServer() {
+	DNSServer dns = new DNSServer() {
+
+	    public Collection findMXRecords(String hostname) {
+		Collection mx = new ArrayList();
+
+		if (hostname.equals(INVALID_HOST)) {
+		    mx.add(INVALID_MX);
+		}
+		return mx;
+	    }
+
+	    public Collection findTXTRecords(String hostname) {
+		throw new UnsupportedOperationException(
+			"Unimplemented Stub Method");
+	    }
+
+	    public InetAddress[] getAllByName(String host)
+		    throws UnknownHostException {
+		throw new UnsupportedOperationException(
+			"Unimplemented Stub Method");
+	    }
+
+	    public InetAddress getByName(String host)
+		    throws UnknownHostException {
+		if (host.equals(INVALID_MX)) {
+		    return InetAddress.getByName(LOOPBACK);
+		}
+		throw new UnknownHostException("Unknown host");
+	    }
+
+	    public Iterator getSMTPHostAddresses(String domainName) {
+		throw new UnsupportedOperationException(
+			"Unimplemented Stub Method");
+	    }
+
+	};
+
+	return dns;
+
+    }
+
+    public void testRejectLoopbackMX() throws ParseException {
+	ValidRcptMX handler = new ValidRcptMX();
+
+	ContainerUtil.enableLogging(handler, new MockLogger());
+
+	handler.setDNSServer(setupMockedDNSServer());
+	handler.onCommand(setupMockedSMTPSession(new MailAddress("test@"
+		+ INVALID_HOST)));
+
+	assertNotNull("Reject", response);
+    }
+
+}
