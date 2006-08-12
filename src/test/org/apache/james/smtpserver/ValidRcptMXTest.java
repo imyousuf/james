ee24088d9ec751d3c54ab8513d49edb56df78b01
Norman Maurer <norman@apache.org>
2006-08-12 14:43:44 +0000

    Formattin
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@431040 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/james/smtpserver/ValidRcptMXTest.java
+++ src/test/org/apache/james/smtpserver/ValidRcptMXTest.java
@@ -47,87 +47,79 @@ public class ValidRcptMXTest extends TestCase {
     private final static String LOOPBACK = "127.0.0.1";
 
     protected void setUp() throws Exception {
-	response = null;
+        response = null;
 
-	super.setUp();
+        super.setUp();
     }
 
     private SMTPSession setupMockedSMTPSession(final MailAddress rcpt) {
-	SMTPSession session = new AbstractSMTPSession() {
-	    HashMap state = new HashMap();
+        SMTPSession session = new AbstractSMTPSession() {
+            HashMap state = new HashMap();
 
-	    public Map getState() {
-		state.put(SMTPSession.CURRENT_RECIPIENT, rcpt);
-		return state;
-	    }
+            public Map getState() {
+                state.put(SMTPSession.CURRENT_RECIPIENT, rcpt);
+                return state;
+            }
 
-	    public void writeResponse(String resp) {
-		response = resp;
-	    }
+            public void writeResponse(String resp) {
+                response = resp;
+            }
 
-	};
-	return session;
+        };
+        return session;
     }
 
     private DNSServer setupMockedDNSServer() {
-	DNSServer dns = new DNSServer() {
+        DNSServer dns = new DNSServer() {
 
-	    public Collection findMXRecords(String hostname) {
-		Collection mx = new ArrayList();
+            public Collection findMXRecords(String hostname) {
+                Collection mx = new ArrayList();
 
-		if (hostname.equals(INVALID_HOST)) {
-		    mx.add(INVALID_MX);
-		}
-		return mx;
-	    }
+                if (hostname.equals(INVALID_HOST)) {
+                    mx.add(INVALID_MX);
+                }
+                return mx;
+            }
 
-	    public Collection findTXTRecords(String hostname) {
-		throw new UnsupportedOperationException(
-			"Unimplemented Stub Method");
-	    }
+            public Collection findTXTRecords(String hostname) {
+                throw new UnsupportedOperationException("Unimplemented Stub Method");
+            }
 
-	    public InetAddress[] getAllByName(String host)
-		    throws UnknownHostException {
-		throw new UnsupportedOperationException(
-			"Unimplemented Stub Method");
-	    }
+            public InetAddress[] getAllByName(String host) throws UnknownHostException {
+                throw new UnsupportedOperationException("Unimplemented Stub Method");
+            }
 
-	    public InetAddress getByName(String host)
-		    throws UnknownHostException {
-		System.err.println("host: " + host);
-		if (host.equals(INVALID_MX) || host.equals(LOOPBACK)) {
-		    return InetAddress.getByName(LOOPBACK);
-		} else if (host.equals("255.255.255.255")) {
-		    return InetAddress.getByName("255.255.255.255");
-		}
-		throw new UnknownHostException("Unknown host");
-	    }
+            public InetAddress getByName(String host) throws UnknownHostException {
+                if (host.equals(INVALID_MX) || host.equals(LOOPBACK)) {
+                    return InetAddress.getByName(LOOPBACK);
+                } else if (host.equals("255.255.255.255")) {
+                    return InetAddress.getByName("255.255.255.255");
+                }
+                throw new UnknownHostException("Unknown host");
+            }
 
-	    public Iterator getSMTPHostAddresses(String domainName) {
-		throw new UnsupportedOperationException(
-			"Unimplemented Stub Method");
-	    }
+            public Iterator getSMTPHostAddresses(String domainName) {
+                throw new UnsupportedOperationException("Unimplemented Stub Method");
+            }
 
-	};
+        };
 
-	return dns;
-
+        return dns;
     }
 
     public void testRejectLoopbackMX() throws ParseException {
-	Collection bNetworks = new ArrayList();
-	bNetworks.add("127.0.0.1");
-	DNSServer dns = setupMockedDNSServer();
-	ValidRcptMX handler = new ValidRcptMX();
+        Collection bNetworks = new ArrayList();
+        bNetworks.add("127.0.0.1");
+        
+        DNSServer dns = setupMockedDNSServer();
+        ValidRcptMX handler = new ValidRcptMX();
 
-	ContainerUtil.enableLogging(handler, new MockLogger());
+        ContainerUtil.enableLogging(handler, new MockLogger());
 
-	handler.setDNSServer(dns);
-	handler.setBannedNetworks(bNetworks, dns);
-	handler.onCommand(setupMockedSMTPSession(new MailAddress("test@"
-		+ INVALID_HOST)));
+        handler.setDNSServer(dns);
+        handler.setBannedNetworks(bNetworks, dns);
+        handler.onCommand(setupMockedSMTPSession(new MailAddress("test@" + INVALID_HOST)));
 
-	assertNotNull("Reject", response);
+        assertNotNull("Reject", response);
     }
-
 }
