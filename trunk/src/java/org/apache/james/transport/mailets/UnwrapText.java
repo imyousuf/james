f36438f64391fa83489b4c87c039249af2e81115
Vincenzo Gianferrari Pini <vincenzo@apache.org>
2006-09-12 15:11:02 +0000

    Fixing tabs to spaces.
    git-svn-id: https://svn.apache.org/repos/asf/james/server@442614 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/java/org/apache/james/transport/mailets/UnwrapText.java
+++ trunk/src/java/org/apache/james/transport/mailets/UnwrapText.java
@@ -31,117 +31,117 @@ import java.util.regex.Pattern;
  *  quotewidth = -10
  */
 public class UnwrapText extends GenericMailet {
-	public final static String PARAMETER_NAME_QUOTEWIDTH = "quotewidth";
-	
-	private int quotewidth;
-	
-	/**
-	 * returns a String describing this mailet.
-	 * 
-	 * @return A desciption of this mailet
-	 */
-	public String getMailetInfo() {
-		return "UnwrapText";
-	}
+    public final static String PARAMETER_NAME_QUOTEWIDTH = "quotewidth";
+    
+    private int quotewidth;
+    
+    /**
+     * returns a String describing this mailet.
+     * 
+     * @return A desciption of this mailet
+     */
+    public String getMailetInfo() {
+        return "UnwrapText";
+    }
 
-	public void init() throws MailetException {
-		quotewidth = Integer.parseInt(getInitParameter(PARAMETER_NAME_QUOTEWIDTH, "-10"));
-	}
+    public void init() throws MailetException {
+        quotewidth = Integer.parseInt(getInitParameter(PARAMETER_NAME_QUOTEWIDTH, "-10"));
+    }
 
-	public void service(Mail mail) throws MailetException {
-		try {
-			// TODO replace non standard quotes (at least "> " with ">", otherwise the widely used  "> > >" will not work.
-			
-			if (FlowedMessageUtils.isFlowedTextMessage(mail.getMessage()))
-				FlowedMessageUtils.deflowMessage(mail.getMessage());
-			
-			else {
-				Object o = mail.getMessage().getContent();
-				if (o instanceof String) {
-					String unwrapped = unwrap((String) o, quotewidth);
-					mail.getMessage().setContent(unwrapped, mail.getMessage().getContentType());
-					mail.getMessage().saveChanges();
-				}
-			}
-			
-		} catch (MessagingException e) {
-			throw new MailetException("Could not unwrap message", e);
-			
-		} catch (IOException e) {
-			throw new MailetException("Could not unwrap message", e);
-		}
-		
-	}
-	
-	public static String unwrap(String text) {
-		return unwrap(text, - 10);
-	}
+    public void service(Mail mail) throws MailetException {
+        try {
+            // TODO replace non standard quotes (at least "> " with ">", otherwise the widely used  "> > >" will not work.
+            
+            if (FlowedMessageUtils.isFlowedTextMessage(mail.getMessage()))
+                FlowedMessageUtils.deflowMessage(mail.getMessage());
+            
+            else {
+                Object o = mail.getMessage().getContent();
+                if (o instanceof String) {
+                    String unwrapped = unwrap((String) o, quotewidth);
+                    mail.getMessage().setContent(unwrapped, mail.getMessage().getContentType());
+                    mail.getMessage().saveChanges();
+                }
+            }
+            
+        } catch (MessagingException e) {
+            throw new MailetException("Could not unwrap message", e);
+            
+        } catch (IOException e) {
+            throw new MailetException("Could not unwrap message", e);
+        }
+        
+    }
+    
+    public static String unwrap(String text) {
+        return unwrap(text, - 10);
+    }
 
-	public static String unwrap(String text, int qwidth) {
-		String[] lines = text.split("\r\n|\n", -1);
-		
-		//P1: Manage spaces without trims
-		Pattern p1 = Pattern.compile("([> ]*)(.*[^ .?!][ ]*)$", 0);
-		
-		//P2: Quotation char at the begin of a line and the first word starts with a lowercase char or a number. The word ends with a space, a tab or a lineend. 
-		Pattern p2 = Pattern.compile("^([> ]*)(([a-zטיאשלע][^ \t\r\n]*|[0-9][0-9,.]*)([ \t].*$|$))", 0);
-		
-		// Width computation
-		int width = 0;
-		for (int i = 0; i < lines.length - 1; i++) {
-			String l = lines[i].trim();
-			if (l.length() > width) width = l.length();
-		}
-		
-		if (width < 40) return text;
-		if (qwidth < 0) qwidth = width - qwidth;
-		
-		StringBuffer result = new StringBuffer();
-		int prevWrapped = 0;
-		for (int i = 0; i < lines.length; i++) {
-			if (prevWrapped != 0) {
-				if (prevWrapped > 0 ) {
-					if (result.charAt(result.length() - 1) != ' ') result.append(" ");
-				}
-				else result.append("\r\n");
-			}
-			String l = lines[i];
-			Matcher m1 = p1.matcher(l);
-			Matcher m2 = i < lines.length - 1 ? p2.matcher(lines[i + 1]) : null;
-			boolean b;
-			int w;
-			// if patterns match, the quote level are identical and if the line length added to the length of the following word is greater than width then it is a wrapped line.
-			if (m1.matches() && i < lines.length - 1 && m2.matches() && (
-					// The following line has the same quoting of the previous.
-					((b = m1.group(1).trim().equals(m2.group(1).trim())) && l.length() + m2.group(3).length() + 1 > width)
-					||
-					// The following line has no quoting (while the previous yes)
-					(!b && m2.group(1).trim().equals("") && (w = l.length() + m2.group(2).trim().length() + 1) > width && w <= qwidth)
-				)) {
-				
-				if (b) {
-					if (prevWrapped > 0 && m1.groupCount() >= 2) result.append(m1.group(2));
-					else result.append(l);
-					prevWrapped = 1;
-					
-				} else {
-					lines[i + 1] = l + (l.charAt(l.length() - 1) != ' ' ? " " : "") + m2.group(2).trim();
-					// Revert the previous append
-					if (prevWrapped != 0) {
-						if (prevWrapped > 0) result.deleteCharAt(result.length() - 1);
-						else result.delete(result.length() - 2, result.length());
-					}
-				}
-				
-			} else {
-				Matcher m3 = p2.matcher(l);
-				if (prevWrapped > 0 && m3.matches()) result.append(m3.group(2));
-				else result.append(lines[i]);
-				prevWrapped = -1;
-			}
-		}
-		
-		return result.toString();
-	}
-	
+    public static String unwrap(String text, int qwidth) {
+        String[] lines = text.split("\r\n|\n", -1);
+        
+        //P1: Manage spaces without trims
+        Pattern p1 = Pattern.compile("([> ]*)(.*[^ .?!][ ]*)$", 0);
+        
+        //P2: Quotation char at the begin of a line and the first word starts with a lowercase char or a number. The word ends with a space, a tab or a lineend. 
+        Pattern p2 = Pattern.compile("^([> ]*)(([a-zטיאשלע][^ \t\r\n]*|[0-9][0-9,.]*)([ \t].*$|$))", 0);
+        
+        // Width computation
+        int width = 0;
+        for (int i = 0; i < lines.length - 1; i++) {
+            String l = lines[i].trim();
+            if (l.length() > width) width = l.length();
+        }
+        
+        if (width < 40) return text;
+        if (qwidth < 0) qwidth = width - qwidth;
+        
+        StringBuffer result = new StringBuffer();
+        int prevWrapped = 0;
+        for (int i = 0; i < lines.length; i++) {
+            if (prevWrapped != 0) {
+                if (prevWrapped > 0 ) {
+                    if (result.charAt(result.length() - 1) != ' ') result.append(" ");
+                }
+                else result.append("\r\n");
+            }
+            String l = lines[i];
+            Matcher m1 = p1.matcher(l);
+            Matcher m2 = i < lines.length - 1 ? p2.matcher(lines[i + 1]) : null;
+            boolean b;
+            int w;
+            // if patterns match, the quote level are identical and if the line length added to the length of the following word is greater than width then it is a wrapped line.
+            if (m1.matches() && i < lines.length - 1 && m2.matches() && (
+                    // The following line has the same quoting of the previous.
+                    ((b = m1.group(1).trim().equals(m2.group(1).trim())) && l.length() + m2.group(3).length() + 1 > width)
+                    ||
+                    // The following line has no quoting (while the previous yes)
+                    (!b && m2.group(1).trim().equals("") && (w = l.length() + m2.group(2).trim().length() + 1) > width && w <= qwidth)
+                )) {
+                
+                if (b) {
+                    if (prevWrapped > 0 && m1.groupCount() >= 2) result.append(m1.group(2));
+                    else result.append(l);
+                    prevWrapped = 1;
+                    
+                } else {
+                    lines[i + 1] = l + (l.charAt(l.length() - 1) != ' ' ? " " : "") + m2.group(2).trim();
+                    // Revert the previous append
+                    if (prevWrapped != 0) {
+                        if (prevWrapped > 0) result.deleteCharAt(result.length() - 1);
+                        else result.delete(result.length() - 2, result.length());
+                    }
+                }
+                
+            } else {
+                Matcher m3 = p2.matcher(l);
+                if (prevWrapped > 0 && m3.matches()) result.append(m3.group(2));
+                else result.append(lines[i]);
+                prevWrapped = -1;
+            }
+        }
+        
+        return result.toString();
+    }
+    
 }
--- trunk/src/java/org/apache/james/transport/mailets/WrapText.java
+++ trunk/src/java/org/apache/james/transport/mailets/WrapText.java
@@ -13,44 +13,44 @@ import java.io.IOException;
  * Convert a message to format=flowed
  */
 public class WrapText extends GenericMailet {
-	private static final String PARAMETER_NAME_FLOWED_DELSP = "delsp";
-	private static final String PARAMETER_NAME_WIDTH = "width";
-	
-	private boolean optionFlowedDelsp = false;
-	private int optionWidth = FlowedMessageUtils.RFC2646_WIDTH;
-	
-	/**
-	 * returns a String describing this mailet.
-	 * 
-	 * @return A desciption of this mailet
-	 */
-	public String getMailetInfo() {
-		return "WrapText";
-	}
+    private static final String PARAMETER_NAME_FLOWED_DELSP = "delsp";
+    private static final String PARAMETER_NAME_WIDTH = "width";
+    
+    private boolean optionFlowedDelsp = false;
+    private int optionWidth = FlowedMessageUtils.RFC2646_WIDTH;
+    
+    /**
+     * returns a String describing this mailet.
+     * 
+     * @return A desciption of this mailet
+     */
+    public String getMailetInfo() {
+        return "WrapText";
+    }
 
-	private static boolean getBooleanParameter(String v, boolean def) {
-		return def ? 
-				!(v != null && (v.equalsIgnoreCase("false") || v.equalsIgnoreCase("no"))) : 
-					v != null && (v.equalsIgnoreCase("true") || v.equalsIgnoreCase("yes"))	;
-	}
-	
-	public void init() throws MailetException {
-		optionFlowedDelsp = getBooleanParameter(getInitParameter(PARAMETER_NAME_FLOWED_DELSP), optionFlowedDelsp);
-		optionWidth = Integer.parseInt(getInitParameter(PARAMETER_NAME_WIDTH, "" + optionWidth));
-	}
+    private static boolean getBooleanParameter(String v, boolean def) {
+        return def ? 
+                !(v != null && (v.equalsIgnoreCase("false") || v.equalsIgnoreCase("no"))) : 
+                    v != null && (v.equalsIgnoreCase("true") || v.equalsIgnoreCase("yes"))  ;
+    }
+    
+    public void init() throws MailetException {
+        optionFlowedDelsp = getBooleanParameter(getInitParameter(PARAMETER_NAME_FLOWED_DELSP), optionFlowedDelsp);
+        optionWidth = Integer.parseInt(getInitParameter(PARAMETER_NAME_WIDTH, "" + optionWidth));
+    }
 
-	public void service(Mail mail) throws MailetException {
-		// TODO We could even manage the flow when the message is quoted-printable
-		
-		try {
-			FlowedMessageUtils.flowMessage(mail.getMessage(), optionFlowedDelsp, optionWidth);
-			
-		} catch (MessagingException e) {
-			throw new MailetException("Could not wrap message", e);
-			
-		} catch (IOException e) {
-			throw new MailetException("Could not wrap message", e);
-		}
-		
-	}
+    public void service(Mail mail) throws MailetException {
+        // TODO We could even manage the flow when the message is quoted-printable
+        
+        try {
+            FlowedMessageUtils.flowMessage(mail.getMessage(), optionFlowedDelsp, optionWidth);
+            
+        } catch (MessagingException e) {
+            throw new MailetException("Could not wrap message", e);
+            
+        } catch (IOException e) {
+            throw new MailetException("Could not wrap message", e);
+        }
+        
+    }
 }
