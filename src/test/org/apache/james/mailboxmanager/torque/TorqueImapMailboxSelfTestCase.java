f066de2abce9b2376deb369bd0b0873f8f276b90
Stefano Bagnara <bago@apache.org>
2006-10-16 23:57:12 +0000

    merging imap-integration branch
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@464747 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/james/mailboxmanager/torque/TorqueImapMailboxSelfTestCase.java
@@ -0,0 +1,31 @@
+package org.apache.james.mailboxmanager.torque;
+
+import javax.mail.MessagingException;
+
+import org.apache.james.mailboxmanager.AbstractImapMailboxSelfTestCase;
+import org.apache.james.mailboxmanager.MailboxManagerException;
+import org.apache.james.mailboxmanager.manager.MailboxManagerProvider;
+import org.apache.james.mailboxmanager.mock.MockUser;
+import org.apache.james.mailboxmanager.mock.TorqueMailboxManagerProviderSingleton;
+import org.apache.torque.TorqueException;
+
+public class TorqueImapMailboxSelfTestCase extends
+        AbstractImapMailboxSelfTestCase {
+    
+    public TorqueImapMailboxSelfTestCase() throws TorqueException {
+        super();
+    }
+
+    public void setUp() throws Exception {
+        MailboxManagerProvider mailboxManagerProvider=TorqueMailboxManagerProviderSingleton.getTorqueMailboxManagerProviderInstance();
+        mailboxManagerProvider.deleteEverything();
+        mailboxManager = mailboxManagerProvider .getGeneralManagerInstance(new MockUser());
+        
+        super.setUp();
+    }
+    
+    public void testAddedEvents() throws MailboxManagerException, MessagingException {
+        super.testAddedEvents();
+    }
+
+}
