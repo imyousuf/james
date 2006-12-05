97374c7296c9a6f2408c27d66dc216092cdb5277
Joachim Draeger <joachim@apache.org>
2006-12-05 21:11:01 +0000

    fixed search (was null) + ASF Header
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@482782 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/james/mailboxmanager/wrapper/ImapMailboxSessionWrapper.java
+++ src/java/org/apache/james/mailboxmanager/wrapper/ImapMailboxSessionWrapper.java
@@ -1,9 +1,27 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
 package org.apache.james.mailboxmanager.wrapper;
 
 import javax.mail.search.SearchTerm;
 
 import org.apache.james.mailboxmanager.GeneralMessageSet;
-import org.apache.james.mailboxmanager.MailboxListener;
 import org.apache.james.mailboxmanager.MailboxManagerException;
 import org.apache.james.mailboxmanager.MessageResult;
 import org.apache.james.mailboxmanager.Quota;
@@ -11,6 +29,7 @@ import org.apache.james.mailboxmanager.acl.MailboxRights;
 import org.apache.james.mailboxmanager.impl.MailboxEventDispatcher;
 import org.apache.james.mailboxmanager.mailbox.ImapMailbox;
 import org.apache.james.mailboxmanager.mailbox.ImapMailboxSession;
+import org.apache.james.mailboxmanager.mailbox.SearchableMailbox;
 
 public class ImapMailboxSessionWrapper extends FlaggedSessionMailboxWrapper
         implements ImapMailboxSession {
@@ -46,9 +65,8 @@ public class ImapMailboxSessionWrapper extends FlaggedSessionMailboxWrapper
         return ((ImapMailbox) mailbox).getUidNext();
     }
 
-    public MessageResult[] search(GeneralMessageSet set, SearchTerm searchTerm, int result) {
-        // TODO Auto-generated method stub
-        return null;
+    public MessageResult[] search(GeneralMessageSet set, SearchTerm searchTerm, int result) throws MailboxManagerException {
+        return addMsnToResults(((SearchableMailbox)mailbox).search(set, searchTerm, noMsnResult(result)),result);
     }
 
 
