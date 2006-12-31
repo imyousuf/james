0cccffa1496deeea7092d5a46769b1f0cc6ad531
Stefano Bagnara <bago@apache.org>
2006-12-31 17:54:30 +0000

    Remove unused code from Util, added few TODOs for the branch experiments.
    git-svn-id: https://svn.apache.org/repos/asf/james/server@491459 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/handlerapi-experiment/src/test/org/apache/james/test/util/Util.java
+++ sandbox/handlerapi-experiment/src/test/org/apache/james/test/util/Util.java
@@ -22,19 +22,6 @@ package org.apache.james.test.util;
 import org.apache.avalon.cornerstone.blocks.datasources.DefaultDataSourceSelector;
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.DefaultConfiguration;
-import org.apache.james.smtpserver.core.AuthCmdHandler;
-import org.apache.james.smtpserver.core.DataCmdHandler;
-import org.apache.james.smtpserver.core.EhloCmdHandler;
-import org.apache.james.smtpserver.core.ExpnCmdHandler;
-import org.apache.james.smtpserver.core.HeloCmdHandler;
-import org.apache.james.smtpserver.core.HelpCmdHandler;
-import org.apache.james.smtpserver.core.MailCmdHandler;
-import org.apache.james.smtpserver.core.QuitCmdHandler;
-import org.apache.james.smtpserver.core.RcptCmdHandler;
-import org.apache.james.smtpserver.core.RsetCmdHandler;
-import org.apache.james.smtpserver.core.SendMailHandler;
-import org.apache.james.smtpserver.core.VrfyCmdHandler;
-import org.apache.james.smtpserver.core.WelcomeMessageHandler;
 import org.apache.james.test.mock.avalon.MockLogger;
 import org.apache.james.test.mock.james.MockMailServer;
 import org.apache.james.test.mock.javaxmail.MockMimeMessage;
@@ -113,35 +100,6 @@ public class Util {
     public static DefaultConfiguration createRemoteManagerHandlerChainConfiguration() {
         DefaultConfiguration handlerChainConfig = new DefaultConfiguration("test");
         return handlerChainConfig;
-    }
-    public static DefaultConfiguration createSMTPHandlerChainConfiguration() {
-        DefaultConfiguration handlerChainConfig = new DefaultConfiguration("handlerchain");
-        handlerChainConfig.addChild(createCommandHandlerConfiguration("HELO", HeloCmdHandler.class));
-        handlerChainConfig.addChild(createCommandHandlerConfiguration("EHLO", EhloCmdHandler.class));
-        handlerChainConfig.addChild(createCommandHandlerConfiguration("AUTH", AuthCmdHandler.class));
-        handlerChainConfig.addChild(createCommandHandlerConfiguration("VRFY", VrfyCmdHandler.class));
-        handlerChainConfig.addChild(createCommandHandlerConfiguration("EXPN", ExpnCmdHandler.class));
-        handlerChainConfig.addChild(createCommandHandlerConfiguration("MAIL", MailCmdHandler.class));
-        handlerChainConfig.addChild(createCommandHandlerConfiguration("RCPT", RcptCmdHandler.class));
-        handlerChainConfig.addChild(createCommandHandlerConfiguration("DATA", DataCmdHandler.class));
-        handlerChainConfig.addChild(createCommandHandlerConfiguration("RSET", RsetCmdHandler.class));
-        handlerChainConfig.addChild(createCommandHandlerConfiguration("HELP", HelpCmdHandler.class));
-        handlerChainConfig.addChild(createCommandHandlerConfiguration("QUIT", QuitCmdHandler.class));
-        // mail sender
-        handlerChainConfig.addChild(createCommandHandlerConfiguration(null, SendMailHandler.class));
-        // welcome message handler
-        handlerChainConfig.addChild(createCommandHandlerConfiguration(null, WelcomeMessageHandler.class));
-        return handlerChainConfig;
-    }
-
-    private static DefaultConfiguration createCommandHandlerConfiguration(String command, Class commandClass) {
-        DefaultConfiguration cmdHandlerConfig = new DefaultConfiguration("handler");
-        if (command != null) {
-            cmdHandlerConfig.setAttribute("command", command);
-        }
-        String classname = commandClass.getName();
-        cmdHandlerConfig.setAttribute("class", classname);
-        return cmdHandlerConfig;
     }
 
     public static MockMail createMockMail2Recipients(MimeMessage m) throws ParseException {
