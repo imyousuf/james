297c7a684092018817223b80fd8ffaf329d2d931
Bernd Fondermann <berndf@apache.org>
2007-10-21 15:05:16 +0000

    moving experimental spring-deployment module into TRUNK
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@586907 13f79535-47bb-0310-9956-ffa450edef68

---
+++ spring-deployment/src/main/java/org/apache/james/container/spring/adaptor/AvalonConfigurationFileProvider.java
@@ -0,0 +1,91 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+package org.apache.james.container.spring.adaptor;
+
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.phoenix.tools.configuration.ConfigurationBuilder;
+import org.apache.james.container.spring.configuration.ConfigurationInterceptor;
+import org.springframework.core.io.Resource;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.beans.BeansException;
+import org.xml.sax.InputSource;
+
+import java.util.Iterator;
+import java.util.List;
+import java.io.InputStream;
+import java.io.IOException;
+
+/**
+ * loads the well-known classic James configuration file
+ *
+  * TODO make this thing be based on Resource class and inject resource.getInputStream() into InputSource 
+ */
+public class AvalonConfigurationFileProvider implements ConfigurationProvider, ApplicationContextAware {
+
+    private String absoluteFilePath;
+    private List configurationInterceptors;
+    private ApplicationContext applicationContext;
+    private String configuration;
+
+    public void setConfigurationResource(String configuration) {
+        this.configuration = configuration;
+    }
+
+
+    public Configuration getConfiguration() {
+        Resource resource = applicationContext.getResource(configuration);
+        InputStream inputStream;
+        try {
+            inputStream = applicationContext.getResource(configuration).getInputStream();
+        } catch (IOException e) {
+            throw new RuntimeException("could not locate configuration file " + configuration, e);
+        }
+        InputSource inputSource = new InputSource(inputStream);
+        Configuration configuration;
+        try
+        {
+            configuration = ConfigurationBuilder.build(inputSource, null, null);
+        }
+        catch( final Exception e )
+        {
+//            getLogger().error( message, e );
+            throw new RuntimeException("failed loading configuration ", e);
+        }
+
+        // apply all interceptors
+        if (configuration != null && configurationInterceptors != null) {
+            Iterator interceptorsIterator = configurationInterceptors.iterator();
+            while (interceptorsIterator.hasNext()) {
+                ConfigurationInterceptor configurationInterceptor = (ConfigurationInterceptor) interceptorsIterator.next();
+                configuration = configurationInterceptor.intercept(configuration);
+            }
+        }
+        
+        return configuration;
+    }
+
+    public void setConfigurationInterceptors(List configurationInterceptors) {
+        this.configurationInterceptors = configurationInterceptors;
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+}
---
+++ spring-deployment/src/main/java/org/apache/james/container/spring/adaptor/FileSystemBridge.java
@@ -0,0 +1,75 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+package org.apache.james.container.spring.adaptor;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.FileInputStream;
+
+import org.apache.james.services.FileSystem;
+
+public class FileSystemBridge implements FileSystem {
+    private static final String FILE_PROTOCOL = "file://";
+    private static final String FILE_PROTOCOL_AND_CONF = "file://conf/";
+    private static final String FILE_PROTOCOL_AND_VAR = "file://var/";
+
+    public File getBasedir() throws FileNotFoundException {
+        return new File(".");
+    }
+
+    /**
+     * loads resources from classpath or file system 
+     */
+    public InputStream getResource(String url) throws IOException {
+        if (url.startsWith("classpath:")) {
+            String resourceName = url.substring("classpath:".length());
+            InputStream resourceAsStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(resourceName);
+            if (resourceAsStream==null) {
+                throw new IOException("Resource '" + resourceName + "' not found in the classpath!");
+            }
+            return resourceAsStream;
+        }
+        return new FileInputStream(getFile(url));
+    }
+
+    /**
+     * @see org.apache.james.services.FileSystem#getFile(String filURL) 
+     */
+    public File getFile(String fileURL) throws FileNotFoundException {
+        if (fileURL.startsWith(FILE_PROTOCOL)) {
+            File file = null;
+            if (fileURL.startsWith(FILE_PROTOCOL_AND_CONF)) {
+                file = new File("../conf/" + fileURL.substring(FILE_PROTOCOL_AND_CONF.length()));
+            } else if (fileURL.startsWith(FILE_PROTOCOL_AND_VAR)) {
+                file = new File("../var/" + fileURL.substring(FILE_PROTOCOL_AND_VAR.length()));
+            } else {
+                file = new File("./" + fileURL.substring(FILE_PROTOCOL.length()));
+            }
+            if (!file.exists()) {
+                throw new FileNotFoundException("cannot access file " + file.toString());
+            }
+            return file;
+        } else {
+            throw new UnsupportedOperationException("getFile: " + fileURL);
+        }
+    }
+
+}
