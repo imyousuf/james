1b32f260ac27df954ca15dc9642b53e6580a6980
Robert Burrell Donkin <rdonkin@apache.org>
2007-10-28 19:28:24 +0000

    More functional tests
    git-svn-id: https://svn.apache.org/repos/asf/james/server@589399 13f79535-47bb-0310-9956-ffa450edef68

---
+++ trunk/phoenix-deployment/src/test/org/apache/james/experimental/imapserver/ExperimentalSelectedStateTest.java
@@ -0,0 +1,66 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.experimental.imapserver;
+
+import org.apache.james.test.functional.imap.AbstractTestSelectedState;
+
+public class ExperimentalSelectedStateTest extends AbstractTestSelectedState {
+
+    public ExperimentalSelectedStateTest() throws Exception {
+        super(HostSystemFactory.createStandardImap());
+    }
+
+    public void testSearch() throws Exception {
+        //TODO Implement search
+    }
+
+    public void testFetchMultipleMessages() throws Exception {
+        // TODO Auto-generated method stub
+        //super.testFetchMultipleMessages();
+    }
+
+    public void testFetchPeek() throws Exception {
+        // STORE FLAGS.SILENT does not work
+        //super.testFetchPeek();
+    }
+
+    public void testFetchSingleMessage() throws Exception {
+        // BODY octet count is buggy.
+        // The total size of the message is used rather than the size of the part
+        // super.testFetchSingleMessage();
+    }
+
+    public void testStore() throws Exception {
+//      STORE FLAGS.SILENT does not work
+//        super.testStore();
+    }
+
+    public void testUid() throws Exception {
+        // UID fetch not working very well
+        // super.testUid();
+    }
+
+    public void testCopy() throws Exception {
+        // extra fetch messages
+    //    super.testCopy();
+    }    
+    
+    
+}
