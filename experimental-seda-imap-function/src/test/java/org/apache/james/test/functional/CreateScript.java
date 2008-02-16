c36066d89553d1d97a685207f1fdf5d9c0aa7903
Robert Burrell Donkin <rdonkin@apache.org>
2008-02-16 13:33:36 +0000

    Some new functional testing scripts for FETCH and the mails I generated them from.
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@628293 13f79535-47bb-0310-9956-ffa450edef68

---
+++ experimental-seda-imap-function/src/test/java/org/apache/james/test/functional/CreateScript.java
@@ -0,0 +1,111 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.test.functional;
+
+public class CreateScript {
+
+    public static final void main(String[] args) throws Exception {
+        ScriptBuilder builder = ScriptBuilder.open("localhost", 143);
+        multipartMixedMessagesPeek(builder);
+    }
+    
+
+    public static void multipartMixedMessagesPeek(ScriptBuilder builder) throws Exception {
+        builder.setPeek(true);
+        multipartMixedMessages(builder);
+    }
+    
+    public static void multipartMixedMessages(ScriptBuilder builder) throws Exception {
+        builder.login();
+        builder.create();
+        builder.select();
+        builder.setFile("multipart-mixed-complex.mail");
+        builder.append();
+        builder.select();
+        builder.fetchSection("");
+        builder.fetchSection("TEXT");
+        builder.fetchSection("HEADER");
+        builder.fetchSection("1");
+        builder.fetchSection("2");
+        builder.fetchSection("3");
+        builder.fetchSection("3.HEADER");
+        builder.fetchSection("3.TEXT");
+        builder.fetchSection("3.1");
+        builder.fetchSection("3.2");
+        builder.fetchSection("4");
+        builder.fetchSection("4.1");
+        builder.fetchSection("4.1.MIME");
+        builder.fetchSection("4.2");
+        builder.fetchSection("4.2.HEADER");
+        builder.fetchSection("4.2.TEXT");
+        builder.fetchSection("4.2.1");
+        builder.fetchSection("4.2.2");
+        builder.fetchSection("4.2.2.1");
+        builder.fetchSection("4.2.2.2");
+        builder.select();
+        builder.quit();
+    }
+    
+    public static void multipartAlternativePeek(ScriptBuilder builder) throws Exception {
+        builder.setPeek(true);
+        multipartAlternative(builder);
+    }
+    
+    public static void multipartAlternative(ScriptBuilder builder) throws Exception {
+        builder.login();
+        builder.create();
+        builder.select();
+        builder.setFile("multipart-alt.mail");
+        builder.append();
+        builder.select();
+        builder.fetchSection("");
+        builder.fetchSection("TEXT");
+        builder.fetchSection("HEADER");
+        builder.fetchSection("1");
+        builder.fetchSection("2");
+        builder.fetchSection("3");
+        builder.select();
+        builder.quit();
+    }
+    
+    public static void multipartMixedPeek(ScriptBuilder builder) throws Exception {
+        builder.setPeek(true);
+        multipartMixed(builder);
+    }
+    
+    public static void multipartMixed(ScriptBuilder builder) throws Exception {
+        builder.login();
+        builder.create();
+        builder.select();
+        builder.setFile("multipart-mixed.mail");
+        builder.append();
+        builder.select();
+        builder.fetchSection("");
+        builder.fetchSection("TEXT");
+        builder.fetchSection("HEADER");
+        builder.fetchSection("1");
+        builder.fetchSection("2");
+        builder.fetchSection("3");
+        builder.fetchSection("4");
+        builder.select();
+        builder.quit();
+    }
+    
+}
