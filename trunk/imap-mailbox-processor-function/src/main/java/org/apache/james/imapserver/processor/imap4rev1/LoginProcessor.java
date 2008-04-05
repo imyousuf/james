e4a276a72e5a783946c2229a0f8af6e2293fde01
Robert Burrell Donkin <rdonkin@apache.org>
2008-04-05 21:39:30 +0000

    Allow three login attempts before termination. The value will be configurable as sooon as the architecture is sorted out.
    git-svn-id: https://svn.apache.org/repos/asf/james/server@645178 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/imap-mailbox-processor-function/src/main/java/org/apache/james/imapserver/processor/imap4rev1/LoginProcessor.java
+++ trunk/imap-mailbox-processor-function/src/main/java/org/apache/james/imapserver/processor/imap4rev1/LoginProcessor.java
@@ -37,6 +37,11 @@ import org.apache.james.services.UsersRepository;
  */
 public class LoginProcessor extends AbstractImapRequestProcessor {
 
+    private static final String ATTRIBUTE_NUMBER_OF_FAILURES 
+        = "org.apache.james.imapserver.processor.imap4rev1.LoginProcessor.NUMBER_OF_FAILURES";
+    // TODO: this should be configurable
+    private static final int MAX_FAILURES = 3;
+    
     private final UsersRepository users;
 
     public LoginProcessor(final ImapProcessor next, final UsersRepository users, 
@@ -60,7 +65,20 @@ public class LoginProcessor extends AbstractImapRequestProcessor {
             ImapSessionUtils.setUser(session, user);
             okComplete(command, tag, responder);
         } else {
-            no(command,tag, responder, HumanReadableTextKey.INVALID_LOGIN);
+            final Integer currentNumberOfFailures = (Integer) session.getAttribute(ATTRIBUTE_NUMBER_OF_FAILURES);
+            final int failures;
+            if (currentNumberOfFailures == null) {
+                failures = 1;
+            } else {
+                failures = currentNumberOfFailures.intValue() + 1;
+            }
+            if (failures < MAX_FAILURES) {
+                session.setAttribute(ATTRIBUTE_NUMBER_OF_FAILURES, new Integer(failures));
+                no(command,tag, responder, HumanReadableTextKey.INVALID_LOGIN);
+            } else {
+                bye(responder, HumanReadableTextKey.TOO_MANY_FAILURES);
+                session.logout();
+            }
         }
     }
 }
