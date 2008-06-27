daf514fdec08aeb345881b940586569b404a038b
Stefano Bagnara <bago@apache.org>
2008-06-27 07:30:38 +0000

    tags cleanup - Moved non-final past releases to "deprecated" folder
    git-svn-id: https://svn.apache.org/repos/asf/james/server/tags/deprecated@672147 13f79535-47bb-0310-9956-ffa450edef68

---
+++ build_2_3_0_B2/src/java/org/apache/james/services/UsersRepository.java
@@ -0,0 +1,159 @@
+/***********************************************************************
+ * Copyright (c) 2000-2006 The Apache Software Foundation.             *
+ * All rights reserved.                                                *
+ * ------------------------------------------------------------------- *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you *
+ * may not use this file except in compliance with the License. You    *
+ * may obtain a copy of the License at:                                *
+ *                                                                     *
+ *     http://www.apache.org/licenses/LICENSE-2.0                      *
+ *                                                                     *
+ * Unless required by applicable law or agreed to in writing, software *
+ * distributed under the License is distributed on an "AS IS" BASIS,   *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or     *
+ * implied.  See the License for the specific language governing       *
+ * permissions and limitations under the License.                      *
+ ***********************************************************************/
+
+package org.apache.james.services;
+
+import java.util.Iterator;
+
+/**
+ * Interface for a repository of users. A repository represents a logical
+ * grouping of users, typically by common purpose. E.g. the users served by an
+ * email server or the members of a mailing list.
+ *
+ *
+ * @version $Revision$
+ */
+public interface UsersRepository {
+
+    /**
+     * The component role used by components implementing this service
+     */
+    String ROLE = "org.apache.james.services.UsersRepository";
+
+    String USER = "USER";
+
+    /**
+     * Adds a user to the repository with the specified User object.
+     *
+     * @param user the user to be added
+     *
+     * @return true if succesful, false otherwise
+     * @since James 1.2.2
+     */
+    boolean addUser(User user);
+
+    /**
+     * Adds a user to the repository with the specified attributes.  In current
+     * implementations, the Object attributes is generally a String password.
+     *
+     * @param name the name of the user to be added
+     * @param attributes see decription
+     */
+    void addUser(String name, Object attributes);
+    
+    /**
+     * Adds a user to the repository with the specified password
+     * 
+     * @param username the username of the user to be added
+     * @param password the password of the user to add
+     * @return true if succesful, false otherwise
+     * 
+     * @since James 2.3.0
+     */
+    boolean addUser(String username, String password);
+
+    /**
+     * Get the user object with the specified user name.  Return null if no
+     * such user.
+     *
+     * @param name the name of the user to retrieve
+     * @return the user being retrieved, null if the user doesn't exist
+     *
+     * @since James 1.2.2
+     */
+    User getUserByName(String name);
+
+    /**
+     * Get the user object with the specified user name. Match user naems on
+     * a case insensitive basis.  Return null if no such user.
+     *
+     * @param name the name of the user to retrieve
+     * @return the user being retrieved, null if the user doesn't exist
+     *
+     * @since James 1.2.2
+     */
+    User getUserByNameCaseInsensitive(String name);
+
+    /**
+     * Returns the user name of the user matching name on an equalsIgnoreCase
+     * basis. Returns null if no match.
+     *
+     * @param name the name to case-correct
+     * @return the case-correct name of the user, null if the user doesn't exist
+     */
+    String getRealName(String name);
+
+    /**
+     * Update the repository with the specified user object. A user object
+     * with this username must already exist.
+     *
+     * @return true if successful.
+     */
+    boolean updateUser(User user);
+
+    /**
+     * Removes a user from the repository
+     *
+     * @param name the user to remove from the repository
+     */
+    void removeUser(String name);
+
+    /**
+     * Returns whether or not this user is in the repository
+     *
+     * @param name the name to check in the repository
+     * @return whether the user is in the repository
+     */
+    boolean contains(String name);
+
+    /**
+     * Returns whether or not this user is in the repository. Names are
+     * matched on a case insensitive basis.
+     *
+     * @param name the name to check in the repository
+     * @return whether the user is in the repository
+     */
+    boolean containsCaseInsensitive(String name);
+
+    /**
+     * Test if user with name 'name' has password 'password'.
+     *
+     * @param name the name of the user to be tested
+     * @param password the password to be tested
+     *
+     * @return true if the test is successful, false if the user
+     *              doesn't exist or if the password is incorrect
+     *
+     * @since James 1.2.2
+     */
+    boolean test(String name, String password);
+
+    /**
+     * Returns a count of the users in the repository.
+     *
+     * @return the number of users in the repository
+     */
+    int countUsers();
+
+    /**
+     * List users in repository.
+     *
+     * @return Iterator over a collection of Strings, each being one user in the repository.
+     */
+    Iterator list();
+
+}
---
+++ build_2_3_0_B2/src/java/org/apache/james/transport/mailets/CommandListservFooter.java
@@ -0,0 +1,136 @@
+/***********************************************************************
+ * Copyright (c) 2000-2006 The Apache Software Foundation.             *
+ * All rights reserved.                                                *
+ * ------------------------------------------------------------------- *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you *
+ * may not use this file except in compliance with the License. You    *
+ * may obtain a copy of the License at:                                *
+ *                                                                     *
+ *     http://www.apache.org/licenses/LICENSE-2.0                      *
+ *                                                                     *
+ * Unless required by applicable law or agreed to in writing, software *
+ * distributed under the License is distributed on an "AS IS" BASIS,   *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or     *
+ * implied.  See the License for the specific language governing       *
+ * permissions and limitations under the License.                      *
+ ***********************************************************************/
+
+package org.apache.james.transport.mailets;
+
+import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.james.util.XMLResources;
+import org.apache.oro.text.regex.MalformedPatternException;
+import org.apache.oro.text.regex.Pattern;
+import org.apache.oro.text.regex.Perl5Compiler;
+import org.apache.oro.text.regex.Perl5Matcher;
+import org.apache.oro.text.regex.StringSubstitution;
+import org.apache.oro.text.regex.Util;
+
+import javax.mail.MessagingException;
+
+
+/**
+ * CommandListservFooter is based on the AddFooter mailet.
+ *
+ * It is used by the {@link CommandListservProcessor} to inject a footer into mailing list.
+ * <br />
+ * <br />
+ *
+ * @version CVS $Revision$ $Date$
+ * @since 2.2.0
+ * @see XMLResources
+ */
+public class CommandListservFooter extends AbstractAddFooter {
+
+    protected String footerText;
+    protected String footerHtml;
+
+    /**
+     * The list serv manager
+     */
+    protected ICommandListservManager commandListservManager;
+
+    /**
+     * For matching
+     */
+    protected Perl5Compiler perl5Compiler = new Perl5Compiler();
+    protected Pattern insertPattern;
+    protected Pattern newlinePattern;
+
+    //For resources
+    protected XMLResources[] xmlResources = new XMLResources[2];
+
+    protected static final int TEXT_PLAIN = 0;
+    protected static final int TEXT_HTML = 1;
+
+    public CommandListservFooter(ICommandListservManager commandListservManager) {
+        this.commandListservManager = commandListservManager;
+        try {
+            insertPattern = perl5Compiler.compile("</body>\\s*</html>", Perl5Compiler.CASE_INSENSITIVE_MASK);
+            newlinePattern = perl5Compiler.compile("\r\n|\n", Perl5Compiler.CASE_INSENSITIVE_MASK);
+        } catch (MalformedPatternException e) {
+            throw new IllegalStateException("Unable to parse regexps: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Initialize the mailet
+     */
+    public void init() throws MessagingException {
+        try {
+            xmlResources = commandListservManager.initXMLResources(new String[]{"footer", "footer_html"});
+        } catch (ConfigurationException e) {
+            throw new MessagingException(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Return a string describing this mailet.
+     *
+     * @return a string describing this mailet
+     */
+    public String getMailetInfo() {
+        return "CommandListservFooter Mailet";
+    }
+
+
+    /**
+     * Get and cache the footer text
+     *
+     * @return the footer text
+     * @see XMLResources
+     */
+    protected String getFooterText() {
+        if (footerText == null) {
+            footerText = getFormattedText(TEXT_PLAIN);
+        }
+        return footerText;
+    }
+
+    /**
+     * Get and cache the footer html text
+     *
+     * @return the footer text
+     * @see XMLResources
+     */
+    protected String getFooterHTML() {
+        if (footerHtml == null) {
+            String footerText = getFormattedText(TEXT_HTML);
+            footerHtml = Util.substitute(new Perl5Matcher(),
+                    newlinePattern,
+                    new StringSubstitution(" <br />"),
+                    footerText,
+                    Util.SUBSTITUTE_ALL);
+        }
+        return footerHtml;
+    }
+
+    /**
+     * @see XMLResources#getString
+     * @param index either {@link #TEXT_PLAIN} or {@link #TEXT_HTML}
+     * @return a formatted text with the proper list and domain
+     */
+    protected String getFormattedText(int index) {
+        return xmlResources[index].getString("text");
+    }
+}
