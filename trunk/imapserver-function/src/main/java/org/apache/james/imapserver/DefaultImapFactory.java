2b994f6463547dc9c28f96f4f37d9be30903f95c
Robert Burrell Donkin <rdonkin@apache.org>
2008-09-16 19:38:47 +0000

    Rationalise avalon bindings
    git-svn-id: https://svn.apache.org/repos/asf/james/server@696024 13f79535-47bb-0310-9956-ffa450edef68

---
+++ trunk/imapserver-function/src/main/java/org/apache/james/imapserver/DefaultImapFactory.java
@@ -0,0 +1,60 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.imapserver;
+
+
+import org.apache.avalon.framework.logger.Logger;
+import org.apache.james.api.imap.process.ImapProcessor;
+import org.apache.james.api.user.UsersRepository;
+import org.apache.james.experimental.imapserver.DefaultImapDecoderFactory;
+import org.apache.james.experimental.imapserver.ImapRequestHandler;
+import org.apache.james.imapserver.codec.decode.ImapDecoder;
+import org.apache.james.imapserver.codec.encode.ImapEncoder;
+import org.apache.james.imapserver.codec.encode.main.DefaultImapEncoderFactory;
+import org.apache.james.imapserver.processor.main.DefaultImapProcessorFactory;
+import org.apache.james.mailboxmanager.impl.DefaultMailboxManagerProvider;
+import org.apache.james.mailboxmanager.torque.DefaultMailboxManager;
+import org.apache.james.mailboxmanager.torque.DefaultUserManager;
+import org.apache.james.services.FileSystem;
+import org.apache.james.user.impl.file.FileUserMetaDataRepository;
+
+public class DefaultImapFactory {
+
+    private final ImapEncoder encoder;
+    private final ImapDecoder decoder;
+    private final ImapProcessor processor;
+    
+    public DefaultImapFactory(FileSystem fileSystem, UsersRepository users, Logger logger) {
+        super();
+        decoder = new DefaultImapDecoderFactory().buildImapDecoder();
+        encoder = new DefaultImapEncoderFactory().buildImapEncoder();
+        processor = DefaultImapProcessorFactory.createDefaultProcessor(
+                new DefaultMailboxManagerProvider(
+                        new DefaultMailboxManager(new DefaultUserManager(
+                                new FileUserMetaDataRepository("var/users"), users), fileSystem, logger)));
+    }
+
+
+
+    public ImapRequestHandler createHandler()
+    { 
+        return new ImapRequestHandler(decoder, processor, encoder);
+    }
+}
--- trunk/imapserver-function/src/main/java/org/apache/james/mailboxmanager/impl/DefaultMailboxManagerProvider.java
+++ trunk/imapserver-function/src/main/java/org/apache/james/mailboxmanager/impl/DefaultMailboxManagerProvider.java
@@ -19,68 +19,21 @@
 
 package org.apache.james.mailboxmanager.impl;
 
-import org.apache.avalon.framework.activity.Initializable;
-import org.apache.avalon.framework.configuration.Configurable;
-import org.apache.avalon.framework.configuration.Configuration;
-import org.apache.avalon.framework.configuration.ConfigurationException;
-import org.apache.avalon.framework.container.ContainerUtil;
-import org.apache.avalon.framework.logger.AbstractLogEnabled;
-import org.apache.avalon.framework.service.ServiceException;
-import org.apache.avalon.framework.service.ServiceManager;
-import org.apache.avalon.framework.service.Serviceable;
 import org.apache.james.mailboxmanager.MailboxManagerException;
 import org.apache.james.mailboxmanager.manager.MailboxManager;
 import org.apache.james.mailboxmanager.manager.MailboxManagerProvider;
 
-public class DefaultMailboxManagerProvider extends AbstractLogEnabled implements MailboxManagerProvider, Configurable, Serviceable, Initializable {
+public class DefaultMailboxManagerProvider implements MailboxManagerProvider {
 
-    public static final char HIERARCHY_DELIMITER = '.';
-
-    public static final String USER_NAMESPACE = "#mail";
-
-    public static final String INBOX = "INBOX";
-
-    private MailboxManager mailboxManager;
-
-    private ServiceManager serviceManager;
+    private final MailboxManager mailboxManager;
+    
+    public DefaultMailboxManagerProvider(final MailboxManager mailboxManager) {
+        super();
+        this.mailboxManager = mailboxManager;
+    }
 
     public MailboxManager getMailboxManager()
             throws MailboxManagerException {
         return mailboxManager;
-    }
-
-    public void configure(Configuration conf) throws ConfigurationException {
-        Configuration factoryConf=conf.getChild("factory",false);
-        String className=factoryConf.getAttribute("class");
-        MailboxManager factory= (MailboxManager) getClassInstace(className);
-        ContainerUtil.enableLogging(factory, getLogger());
-        ContainerUtil.configure(factory, factoryConf);
-        setMailboxManagerInstance(factory);
-    }
-    
-    public void setMailboxManagerInstance(MailboxManager mailboxManager) {
-        this.mailboxManager = mailboxManager;
-    }
-
-    private static Object getClassInstace(String name) {
-        Object object=null;
-        try {
-            Class clazz = Thread.currentThread().getContextClassLoader().loadClass(name);
-            object=clazz.newInstance();
-            return object;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        
-    }
-
-    public void service(ServiceManager serviceManager) throws ServiceException {
-        this.serviceManager=serviceManager;
-        
-    }
-
-    public void initialize() throws Exception {
-        ContainerUtil.service(mailboxManager, serviceManager);
-        ContainerUtil.initialize(mailboxManager);
     }
 }
