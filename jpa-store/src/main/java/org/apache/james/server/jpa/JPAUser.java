0e5a32deabb4a25cdccaae4c23861811dcb55403
Robert Burrell Donkin <rdonkin@apache.org>
2008-11-14 06:36:25 +0000

    Added missing equals and hash code. Thanks to Norman Maurer for the spot.
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@713928 13f79535-47bb-0310-9956-ffa450edef68

--- jpa-store/src/main/java/org/apache/james/server/jpa/JPAUser.java
+++ jpa-store/src/main/java/org/apache/james/server/jpa/JPAUser.java
@@ -102,6 +102,35 @@ public class JPAUser implements User {
         }
         return result;
     }
+
+    @Override
+    public int hashCode() {
+        final int PRIME = 31;
+        int result = 1;
+        result = PRIME * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final JPAUser other = (JPAUser) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
     
+    @Override
+     public String toString() {
+         return "[User " + name + "]";
+     }
     
 }
