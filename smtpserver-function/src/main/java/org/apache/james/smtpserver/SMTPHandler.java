c0da6860f20a60b0e80e9a33238f1da9747d326d
Robert Burrell Donkin <rdonkin@apache.org>
2009-09-06 09:47:53 +0000

    Access to services no longer required by configuration.
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@811789 13f79535-47bb-0310-9956-ffa450edef68

--- smtpserver-function/src/main/java/org/apache/james/smtpserver/SMTPHandler.java
+++ smtpserver-function/src/main/java/org/apache/james/smtpserver/SMTPHandler.java
@@ -28,11 +28,9 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.Random;
 
-import org.apache.james.api.user.UsersRepository;
+import org.apache.james.socket.CRLFDelimitedByteBuffer;
 import org.apache.james.socket.ProtocolHandler;
 import org.apache.james.socket.ProtocolHandlerHelper;
-import org.apache.james.socket.CRLFDelimitedByteBuffer;
-import org.apache.james.services.MailServer;
 
 /**
  * Provides SMTP functionality by carrying out the server side of the SMTP
@@ -396,20 +394,12 @@ public class SMTPHandler implements ProtocolHandler, SMTPSession {
         return getConfigurationData().getHelloName();
     }
 
-    public MailServer getMailServer() {
-        return getConfigurationData().getMailServer();
-    }
-
     public long getMaxMessageSize() {
         return getConfigurationData().getMaxMessageSize();
     }
 
     public String getSMTPGreeting() {
         return getConfigurationData().getSMTPGreeting();
-    }
-
-    public UsersRepository getUsersRepository() {
-        return getConfigurationData().getUsersRepository();
     }
 
     public boolean useAddressBracketsEnforcement() {
