0e367e7b73f16b6ded14450f640524fafece8c6c
Norman Maurer <norman@apache.org>
2009-09-29 15:58:45 +0000

    Missing header
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@820003 13f79535-47bb-0310-9956-ffa450edef68

--- pop3server-function/src/main/java/org/apache/james/pop3server/StlsCmdHandler.java
+++ pop3server-function/src/main/java/org/apache/james/pop3server/StlsCmdHandler.java
@@ -1,3 +1,23 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+
 package org.apache.james.pop3server;
 
 import java.io.IOException;
@@ -5,10 +25,10 @@ import java.io.IOException;
 public class StlsCmdHandler implements CommandHandler{
 
 	/**
-	 * @throws  
-	 * 
+	 * @see org.apache.james.pop3server.CommandHandler#onCommand(org.apache.james.pop3server.POP3Session)
 	 */
 	public void onCommand(POP3Session session)  {
+		// check if starttls is supported, the state is the right one and it was not started before
 		if (session.isStartTLSSupported() && session.getHandlerState() == POP3Handler.TRANSACTION && session.isTLSStarted() == false) {
 			session.writeResponse(POP3Handler.OK_RESPONSE+ " Begin TLS negotiation");
 			try {
