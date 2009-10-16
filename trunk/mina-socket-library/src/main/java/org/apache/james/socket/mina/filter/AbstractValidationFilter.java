0f8bed1e18e2212dbc368f9c791a66242ca216bb
Norman Maurer <norman@apache.org>
2009-10-16 13:17:24 +0000

    Move generic classes for mina based servers to an extra module
    git-svn-id: https://svn.apache.org/repos/asf/james/server@825879 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/mina-socket-library/src/main/java/org/apache/james/socket/mina/filter/AbstractValidationFilter.java
+++ trunk/mina-socket-library/src/main/java/org/apache/james/socket/mina/filter/AbstractValidationFilter.java
@@ -16,18 +16,14 @@
  * specific language governing permissions and limitations      *
  * under the License.                                           *
  ****************************************************************/
-package org.apache.james.smtpserver.mina.filter;
+package org.apache.james.socket.mina.filter;
 
 import org.apache.commons.logging.Log;
-import org.apache.james.smtpserver.protocol.SMTPRequest;
-import org.apache.james.smtpserver.protocol.SMTPResponse;
-import org.apache.james.smtpserver.protocol.SMTPRetCode;
 import org.apache.mina.core.filterchain.IoFilterAdapter;
 import org.apache.mina.core.session.IoSession;
-import org.apache.mina.core.write.DefaultWriteRequest;
 import org.apache.mina.core.write.WriteRequest;
 
-public class AbstractValidationFilter extends IoFilterAdapter {
+public abstract class AbstractValidationFilter extends IoFilterAdapter {
 
     private Log logger;
     
@@ -40,21 +36,70 @@ public class AbstractValidationFilter extends IoFilterAdapter {
     }
 
     /**
+     * @see org.apache.mina.core.filterchain.IoFilterAdapter#messageReceived(org.apache.mina.core.filterchain.IoFilter.NextFilter, org.apache.mina.core.session.IoSession, java.lang.Object)
+     */
+    public void messageReceived(NextFilter nextFilter, IoSession session, Object message) throws Exception {
+        if (isValidRequest(message)) {
+            super.messageReceived(nextFilter, session, message);
+        } else {
+            errorRequest(message);
+        }
+    }
+
+    /**
      * @see org.apache.mina.core.filterchain.IoFilterAdapter#filterWrite(org.apache.mina.core.filterchain.IoFilter.NextFilter, org.apache.mina.core.session.IoSession, org.apache.mina.core.write.WriteRequest)
      */
     public void filterWrite(NextFilter arg0, IoSession arg1, WriteRequest arg2)
             throws Exception {
         Object obj = arg2.getMessage();
 
-        if (obj instanceof SMTPResponse || obj instanceof SMTPRequest) {
+        if (isValidRequest(obj) || isValidResponse(obj)) {
             super.filterWrite(arg0, arg1, arg2);
         } else {
-            logger.error("WriteRequest holds not a SMTPResponseImpl but "
+            logger.error("WriteRequest holds not a an valid Object but "
                     + (obj == null ? "NULL" : obj.getClass()));
-            arg0.filterWrite(arg1, new DefaultWriteRequest(new SMTPResponse(
-                    SMTPRetCode.TRANSACTION_FAILED, "Cannot handle Request")));
+            WriteRequest request = errorResponse(obj);
+            if (request != null) {
+                arg0.filterWrite(arg1, request); 
+            }
         }
     }
 
+    /**
+     * Check if the given Object is valid as Request. If so return true
+     * 
+     * @param requestObject
+     * @return isValidRequest
+     */
+    protected abstract boolean isValidRequest(Object requestObject);
+    
+    
+    /**
+     * Check if the given Object is a valid Response. If so return true
+     * 
+     * @param responseObject
+     * @return isValidResponse
+     */
+    protected abstract boolean isValidResponse(Object responseObject);
+    
+  
+    /**
+     * Return the WriteRequest which should get written to the client. If you don't want to write anything just return
+     * null
+     * 
+     * @param obj the original obj
+     * @return writeRequest
+     */
+    protected abstract WriteRequest errorResponse(Object obj);
+    
+    
+    /**
+     * Return the WriteRequest which should get written to the client. If you don't want to write anything just return
+     * null
+     * 
+     * @param obj the original obj
+     * @return writeRequest
+     */
+    protected abstract WriteRequest errorRequest(Object obj);
 
 }
--- trunk/mina-socket-library/src/main/java/org/apache/james/socket/mina/filter/ConnectionFilter.java
+++ trunk/mina-socket-library/src/main/java/org/apache/james/socket/mina/filter/ConnectionFilter.java
@@ -17,7 +17,7 @@
  * under the License.                                           *
  ****************************************************************/
 
-package org.apache.james.smtpserver.mina.filter;
+package org.apache.james.socket.mina.filter;
 
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
