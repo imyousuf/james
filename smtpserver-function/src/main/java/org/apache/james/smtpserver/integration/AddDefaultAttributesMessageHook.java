fd10dda88c68dbfaad507ebceea8cdd61b999975
Stefano Bagnara <bago@apache.org>
2009-10-18 13:25:39 +0000

    move non generic smtp hooks/handlers to an "integration" package
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@826432 13f79535-47bb-0310-9956-ffa450edef68

--- smtpserver-function/src/main/java/org/apache/james/smtpserver/integration/AddDefaultAttributesMessageHook.java
+++ smtpserver-function/src/main/java/org/apache/james/smtpserver/integration/AddDefaultAttributesMessageHook.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations      *
  * under the License.                                           *
  ****************************************************************/
-package org.apache.james.smtpserver.protocol.core;
+package org.apache.james.smtpserver.integration;
 
 import org.apache.james.core.MailImpl;
 import org.apache.james.smtpserver.protocol.SMTPSession;
