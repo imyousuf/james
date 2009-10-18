e60ea4602400a49701720bb907c23d0b86a0e502
Norman Maurer <norman@apache.org>
2009-10-18 15:51:07 +0000

    POP3BeforeSMTPHandler belongs to integration
    git-svn-id: https://svn.apache.org/repos/asf/james/server@826465 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/smtpserver-function/src/test/java/org/apache/james/smtpserver/POP3BeforeSMTPHandlerTest.java
+++ trunk/smtpserver-function/src/test/java/org/apache/james/smtpserver/POP3BeforeSMTPHandlerTest.java
@@ -21,8 +21,8 @@
 package org.apache.james.smtpserver;
 
 import org.apache.avalon.framework.container.ContainerUtil;
+import org.apache.james.smtpserver.integration.POP3BeforeSMTPHandler;
 import org.apache.james.smtpserver.protocol.SMTPSession;
-import org.apache.james.smtpserver.protocol.core.POP3BeforeSMTPHandler;
 import org.apache.james.test.mock.avalon.MockLogger;
 import org.apache.james.util.POP3BeforeSMTPHelper;
 
