c326f7295dad3b13ff404c68b327843abaad926d
Norman Maurer <norman@apache.org>
2009-11-02 14:01:39 +0000

    move mock class
    git-svn-id: https://svn.apache.org/repos/asf/james/server@831898 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/user-api/src/test/java/org/apache/james/api/vut/management/MockVirtualUserTableManagementService.java
+++ trunk/user-api/src/test/java/org/apache/james/api/vut/management/MockVirtualUserTableManagementService.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations      *
  * under the License.                                           *
  ****************************************************************/
-package org.apache.james.management.mock;
+package org.apache.james.api.vut.management;
 
 import java.util.ArrayList;
 import java.util.Collection;
