55436fe82e26b82b3c4a5c5118c32c8ad52d1639
Norman Maurer <norman@apache.org>
2010-02-01 07:38:55 +0000

    complete merge
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@905200 13f79535-47bb-0310-9956-ffa450edef68

---
+++ user-function/src/test/java/org/apache/james/userrepository/DefaultUsersJdbcRepositoryTest.java
@@ -0,0 +1,86 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.userrepository;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.DefaultConfigurationBuilder;
+import org.apache.commons.logging.impl.SimpleLog;
+import org.apache.james.api.user.UsersRepository;
+import org.apache.james.lifecycle.LifecycleUtil;
+import org.apache.james.services.DataSourceSelector;
+import org.apache.james.test.mock.james.MockFileSystem;
+import org.apache.james.test.util.Util;
+
+import java.util.Iterator;
+
+/**
+ * Test basic behaviours of UsersFileRepository
+ */
+public class DefaultUsersJdbcRepositoryTest extends MockUsersRepositoryTest {
+
+    /**
+     * Create the repository to be tested.
+     * 
+     * @return the user repository
+     * @throws Exception 
+     */
+    protected UsersRepository getUsersRepository() throws Exception {
+        DefaultUsersJdbcRepository res = new DefaultUsersJdbcRepository();
+        String tableString = "defusers";
+        configureAbstractJdbcUsersRepository(res, tableString);
+        return res;
+    }
+
+    /**
+     * @param res
+     * @param tableString
+     * @throws Exception
+     * @throws ConfigurationException
+     */
+    protected void configureAbstractJdbcUsersRepository(AbstractJdbcUsersRepository res, String tableString) throws Exception, ConfigurationException {
+        res.setFileSystem(new MockFileSystem());
+        DataSourceSelector dataSourceSelector = Util.getDataSourceSelector();  
+        
+        res.setDatasources(dataSourceSelector );
+        
+        DefaultConfigurationBuilder configuration = new DefaultConfigurationBuilder();
+        configuration.addProperty("[@destinationURL]", "db://maildb/"+tableString);
+        configuration.addProperty("sqlFile","file://conf/sqlResources.xml");
+        res.setLog(new SimpleLog("MockLog"));
+        res.configure(configuration);
+        res.init();
+    }
+
+    /**
+     * @return
+     */
+    protected boolean getCheckCase() {
+        return true;
+    }
+
+    protected void disposeUsersRepository() {
+        Iterator<String> i = this.usersRepository.list();
+        while (i.hasNext()) {
+            this.usersRepository.removeUser((String) i.next());
+        }
+        LifecycleUtil.dispose(this.usersRepository);
+    }
+
+}
