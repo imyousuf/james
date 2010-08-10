92fa5a318d80abdea7c0b91c60b5e49e95e69b54
Norman Maurer <norman@apache.org>
2010-08-10 18:41:38 +0000

    Add a NetworkIsinWhitelist matcher which match networks against a database. I think it make more sense to put it in a extra matcher(JAMES-1034)
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@984159 13f79535-47bb-0310-9956-ffa450edef68

--- mailets/src/main/java/org/apache/james/transport/matchers/NetworkIsInWhitelist.java
+++ mailets/src/main/java/org/apache/james/transport/matchers/NetworkIsInWhitelist.java
@@ -16,71 +16,71 @@
  * specific language governing permissions and limitations      *
  * under the License.                                           *
  ****************************************************************/
-
 package org.apache.james.transport.matchers;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
+import javax.annotation.Resource;
 import javax.mail.MessagingException;
 
+import org.apache.james.api.dnsservice.DNSService;
+import org.apache.james.api.dnsservice.util.NetMatcher;
 import org.apache.mailet.Mail;
 import org.apache.mailet.MailAddress;
 
 /**
- * <P>
- * Matches recipients having the mail sender in the recipient's private
- * whitelist .
- * </P>
- * <P>
- * The recipient name is always converted to its primary name (handling
- * aliases).
- * </P>
- * <P>
- * Configuration string: The database name containing the white list table.
- * </P>
- * <P>
- * Example:
- * </P>
+ * Matcher which lookup whitelisted networks in a database. The networks can be specified via netmask. 
  * 
- * <PRE>
- * &lt;CODE&gt;
- *    &lt;mailet match=&quot;IsInWhiteList=db://maildb&quot; class=&quot;ToProcessor&quot;&gt;
- *       &lt;processor&gt; transport &lt;/processor&gt;
- *    &lt;/mailet&gt;
- * &lt;/CODE&gt;
- * </PRE>
+ * For example:
+ * 192.168.0.0/24
  * 
- * @see org.apache.james.transport.mailets.WhiteListManager
- * @version SVN $Revision: $ $Date: $
- * @since 2.3.0
+ * Th whitelisting is done per recipient
+ *
  */
-public class IsInWhiteList extends AbstractSQLWhitelistMatcher {
+public class NetworkIsInWhitelist extends AbstractSQLWhitelistMatcher {
 
-    private String selectByPK;
+    private DNSService dns;
+    private String selectNetworks;
 
-    @Override
-    public void init() throws javax.mail.MessagingException {
-        super.init();
-        selectByPK = sqlQueries.getSqlString("selectByPK", true);
+    /**
+     * Injection setter for the DNSService.
+     * 
+     * @param dnsService
+     */
+    @Resource(name="dnsserver")
+    public void setDNSService(DNSService dns) {
+        this.dns = dns;
     }
+    
 
     /*
      * (non-Javadoc)
      * @see org.apache.james.transport.matchers.AbstractSQLWhitelistMatcher#getSQLSectionName()
      */
     protected String getSQLSectionName() {
-        return "Whitelist";
+        return "NetworkWhitelist";
     }
+
+    @Override
+    public void init() throws MessagingException {
+        super.init();
+        selectNetworks = sqlQueries.getSqlString("selectNetwork", true);
+
+    }
+
 
     /*
      * (non-Javadoc)
      * @see org.apache.james.transport.matchers.AbstractSQLWhitelistMatcher#matchedWhitelist(org.apache.mailet.MailAddress, org.apache.mailet.Mail)
      */
     protected boolean matchedWhitelist(MailAddress recipientMailAddress, Mail mail) throws MessagingException {
+        // check if it's a local sender
         MailAddress senderMailAddress = mail.getSender();
         String senderUser = senderMailAddress.getLocalPart().toLowerCase(Locale.US);
         String senderHost = senderMailAddress.getDomain().toLowerCase(Locale.US);
@@ -99,17 +99,19 @@ public class IsInWhiteList extends AbstractSQLWhitelistMatcher {
                 }
 
                 if (selectStmt == null) {
-                    selectStmt = conn.prepareStatement(selectByPK);
+                    selectStmt = conn.prepareStatement(selectNetworks);
                 }
                 selectStmt.setString(1, recipientUser);
                 selectStmt.setString(2, recipientHost);
                 selectStmt.setString(3, senderUser);
                 selectStmt.setString(4, senderHost);
                 selectRS = selectStmt.executeQuery();
-                if (selectRS.next()) {
-                    // This address was already in the list
-                    return true;
+                List<String> nets = new ArrayList<String>();
+                while (selectRS.next()) {
+                    nets.add(selectRS.getString(1));
                 }
+                NetMatcher matcher = new NetMatcher(nets, dns);
+                return matcher.matchInetNetwork(mail.getRemoteAddr());
 
             } finally {
                 theJDBCUtil.closeJDBCResultSet(selectRS);
@@ -121,24 +123,25 @@ public class IsInWhiteList extends AbstractSQLWhitelistMatcher {
         } finally {
             theJDBCUtil.closeJDBCStatement(selectStmt);
             theJDBCUtil.closeJDBCConnection(conn);
-        }
-        return false;
+        }        
     }
+
 
     /*
      * (non-Javadoc)
      * @see org.apache.james.transport.matchers.AbstractSQLWhitelistMatcher#getTableCreateQueryName()
      */
     protected String getTableCreateQueryName() {
-        return "createWhiteListTable";
+        return "createNetworkWhiteListTable";
     }
+
 
     /*
      * (non-Javadoc)
      * @see org.apache.james.transport.matchers.AbstractSQLWhitelistMatcher#getTableName()
      */
     protected String getTableName() {
-        return "whiteListTableName";
+        return "networkWhiteListTableName";
     }
 
 }
