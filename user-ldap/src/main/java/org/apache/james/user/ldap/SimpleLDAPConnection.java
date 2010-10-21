8e1307e7bc379dc070185c38cd93bd89a39e54b0
Eric Charles <eric@apache.org>
2010-10-21 09:41:10 +0000

    Align test packages hierarchy for user/vut service - ldap part (JAMES1074)
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@1025911 13f79535-47bb-0310-9956-ffa450edef68

--- user-ldap/src/main/java/org/apache/james/user/ldap/SimpleLDAPConnection.java
+++ user-ldap/src/main/java/org/apache/james/user/ldap/SimpleLDAPConnection.java
@@ -17,7 +17,7 @@
  * under the License.                                           *
  ****************************************************************/
 
-package org.apache.james.userrepository;
+package org.apache.james.user.ldap;
 
 import java.util.Properties;
 
