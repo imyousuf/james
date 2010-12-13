9e1d8034ec82681df812c1e32a9807b690184bcc
Norman Maurer <norman@apache.org>
2010-12-13 16:52:59 +0000

    Just some adjustment to make it easier to work with spring-dm extender. See JAMES-910
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@1045229 13f79535-47bb-0310-9956-ffa450edef68

--- container-spring/src/main/java/org/apache/james/container/spring/lifecycle/RestrictedLifeCycleBeanPostProcessor.java
+++ container-spring/src/main/java/org/apache/james/container/spring/lifecycle/RestrictedLifeCycleBeanPostProcessor.java
@@ -19,23 +19,24 @@
 package org.apache.james.container.spring.lifecycle;
 
 import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.ListableBeanFactory;
 
 /**
- * {@link AbstractLifeCycleBeanPostProcessor} sub-class which will only try to apply LifeCycle callbacks on beans which are registered on the {@link ApplicationContext}.
+ * {@link AbstractLifeCycleBeanPostProcessor} sub-class which will only try to apply LifeCycle callbacks on beans which are registered on the {@link BeanFactory}.
  * 
  *
  * @param <T>
  */
-public abstract class RestrictedLifeCycleBeanPostProcessor<T> extends AbstractLifeCycleBeanPostProcessor<T> implements ApplicationContextAware{
+public abstract class RestrictedLifeCycleBeanPostProcessor<T> extends AbstractLifeCycleBeanPostProcessor<T> implements BeanFactoryAware{
 
-    private ApplicationContext context;
+    private ListableBeanFactory factory;
 
     @Override
     protected final void executeLifecycleMethodAfterInit(T bean, String beanname) throws Exception {
         // check if the bean is registered in the context. If not it was create by the InstanceFactory and so there is no need to execute the callback
-        if (context.containsBeanDefinition(beanname)) {
+        if (factory.containsBeanDefinition(beanname)) {
             executeLifecycleMethodAfterInitChecked(bean, beanname);
         }
     }
@@ -43,17 +44,18 @@ public abstract class RestrictedLifeCycleBeanPostProcessor<T> extends AbstractLi
     @Override
     protected final void executeLifecycleMethodBeforeInit(T bean, String beanname) throws Exception {
         // check if the bean is registered in the context. If not it was create by the InstanceFactory and so there is no need to execute the callback
-        if (context.containsBeanDefinition(beanname)) {
+        if (factory.containsBeanDefinition(beanname)) {
             executeLifecycleMethodBeforeInitChecked(bean, beanname);
         }
     }
 
+
     /*
      * (non-Javadoc)
-     * @see org.springframework.context.ApplicationContextAware#setApplicationContext(org.springframework.context.ApplicationContext)
+     * @see org.springframework.beans.factory.BeanFactoryAware#setBeanFactory(org.springframework.beans.factory.BeanFactory)
      */
-    public void setApplicationContext(ApplicationContext context) throws BeansException {
-        this.context = context;
+    public void setBeanFactory(BeanFactory factory) throws BeansException {
+        this.factory = (ListableBeanFactory) factory;
     }
 
     /**
