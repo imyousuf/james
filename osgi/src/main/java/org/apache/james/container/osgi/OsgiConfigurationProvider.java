b79267db7bdcf7486a151f958302ed2a8958dfe6
Norman Maurer <norman@apache.org>
2010-12-14 10:23:51 +0000

    Add custom extender bundle which will take care of james lifecycle within OSGI. See JAMES-910
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@1049023 13f79535-47bb-0310-9956-ffa450edef68

---
+++ osgi/src/main/java/org/apache/james/container/osgi/OsgiConfigurationProvider.java
@@ -0,0 +1,54 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+package org.apache.james.container.osgi;
+
+import java.io.File;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.HierarchicalConfiguration;
+import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.james.container.spring.lifecycle.ConfigurationProvider;
+
+/**
+ * Load configuration files from the specified configuration directory
+ * 
+ *
+ */
+public class OsgiConfigurationProvider implements ConfigurationProvider{
+
+    private String configDir;
+
+    public OsgiConfigurationProvider(String configDir) {
+        this.configDir = configDir;
+    }
+    
+    /*
+     * (non-Javadoc)
+     * @see org.apache.james.container.spring.lifecycle.ConfigurationProvider#getConfiguration(java.lang.String)
+     */
+    public HierarchicalConfiguration getConfiguration(String beanName) throws ConfigurationException {
+        XMLConfiguration config = new XMLConfiguration();
+        config.setDelimiterParsingDisabled(true);
+        
+        config.load(new File(configDir +"/" + beanName + ".xml"));        
+        return config;
+        
+    }
+
+}
