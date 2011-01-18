4e33146b48b9683d03d799fe5bb4bb5ea923f700
Stefano Bagnara <bago@apache.org>
2011-01-18 15:20:15 +0000

    Moved MailetContextLog to "mailets" package so to remove commons-logging dependency from "util".
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@1060410 13f79535-47bb-0310-9956-ffa450edef68

--- mailets/src/main/java/org/apache/james/transport/util/MailetContextLog.java
+++ mailets/src/main/java/org/apache/james/transport/util/MailetContextLog.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations      *
  * under the License.                                           *
  ****************************************************************/
-package org.apache.james.util;
+package org.apache.james.transport.util;
 
 import org.apache.commons.logging.Log;
 import org.apache.mailet.MailetContext;
