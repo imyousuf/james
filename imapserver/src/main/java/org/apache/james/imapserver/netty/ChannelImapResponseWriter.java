233acf24fbb81df21e0d322556ad35af4272728c
Norman Maurer <norman@apache.org>
2011-03-31 05:58:20 +0000

    Fix possible deadlock which I committed by mistake
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@1087196 13f79535-47bb-0310-9956-ffa450edef68

--- imapserver/src/main/java/org/apache/james/imapserver/netty/ChannelImapResponseWriter.java
+++ imapserver/src/main/java/org/apache/james/imapserver/netty/ChannelImapResponseWriter.java
@@ -27,7 +27,6 @@ import org.apache.james.imap.main.AbstractImapResponseWriter;
 import org.apache.james.imap.message.response.Literal;
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.Channel;
-import org.jboss.netty.channel.ChannelFuture;
 
 /**
  * {@link AbstractImapResponseWriter} implementation which writes the data to a {@link Channel}
@@ -48,11 +47,7 @@ public class ChannelImapResponseWriter extends AbstractImapResponseWriter{
      * @see org.apache.james.imap.main.AbstractImapResponseWriter#write(java.nio.ByteBuffer)
      */
     protected void write(ByteBuffer buffer) throws IOException {
-        ChannelFuture f = channel.write(ChannelBuffers.wrappedBuffer(buffer)).awaitUninterruptibly();
-        Throwable t = f.getCause();
-        if (t != null) {
-            throw new IOException(t.getMessage());
-        }
+        channel.write(ChannelBuffers.wrappedBuffer(buffer));
     }
 
     /*
