c438deb392314f66f82a06be6842907b4440f21b
Eric Charles <eric@apache.org>
2011-05-25 11:40:34 +0000

    Move images on dev page - bring more information architecture (JAMES-1219)
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@1127477 13f79535-47bb-0310-9956-ffa450edef68

--- src/site/resources/js/dev-extend.js
+++
@@ -1,4 +0,0 @@
-$(function(){
-	  $("#arch-img-id").fancybox();
-	  $("#arch-txt-id").fancybox();
-});
--- src/site/resources/js/dev.js
+++ src/site/resources/js/dev.js
@@ -1,4 +1,6 @@
 $(function(){
 	  $("#high-level-arch-img-id").fancybox();
 	  $("#high-level-arch-txt-id").fancybox();
+	  $("#arch-img-id").fancybox();
+	  $("#arch-txt-id").fancybox();
 });
