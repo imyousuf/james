266e5571ca2aef6ebcbb6f4cc98f09d8924206b7
Robert Burrell Donkin <rdonkin@apache.org>
2011-06-16 18:52:16 +0000

    JAMES-1269 Remove dependency on LGPL findbugs library annotations.
    git-svn-id: https://svn.apache.org/repos/asf/james/server/trunk@1136598 13f79535-47bb-0310-9956-ffa450edef68

--- queue-activemq/src/main/java/org/apache/james/queue/activemq/FileSystemBlobStrategy.java
+++ queue-activemq/src/main/java/org/apache/james/queue/activemq/FileSystemBlobStrategy.java
@@ -59,7 +59,6 @@ public class FileSystemBlobStrategy implements BlobUploadStrategy, BlobDownloadS
      * org.apache.activemq.blob.BlobUploadStrategy#uploadFile(org.apache.activemq
      * .command.ActiveMQBlobMessage, java.io.File)
      */
-    @edu.umd.cs.findbugs.annotations.SuppressWarnings(value = "OBL_UNSATISFIED_OBLIGATION", justification = "Closed in uploadStream")
     public URL uploadFile(ActiveMQBlobMessage message, File file) throws JMSException, IOException {
         return uploadStream(message, new FileInputStream(file));
     }
